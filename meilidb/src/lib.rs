77405cc10375cecf18d367cabdf8f3039f6c6ff8
Clément Renault <renault.cle@gmail.com>
2019-03-29 16:58:55 +0100

    chore: Remove the database module from meilidb

--- meilidb/src/lib.rs
+++ meilidb/src/lib.rs
@@ -1,10 +1,7 @@
 #![cfg_attr(feature = "nightly", feature(test))]
 
-pub mod database;
 mod common_words;
 mod sort_by_attr;
-
-pub use rocksdb;
 
 pub use self::sort_by_attr::SortByAttr;
 pub use self::common_words::CommonWords;
