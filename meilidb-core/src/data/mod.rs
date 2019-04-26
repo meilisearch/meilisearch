27ffcaabe97569ee426279400317c1114bd2c4ff
Clément Renault <renault.cle@gmail.com>
2019-04-26 15:31:00 +0200

    chore: Remove the DocIndexes type

--- meilidb-core/src/data/mod.rs
+++ meilidb-core/src/data/mod.rs
@@ -1,14 +1,3 @@
-mod doc_indexes;
 mod shared_data;
 
-use std::slice::from_raw_parts;
-use std::mem::size_of;
-
-pub use self::doc_indexes::{DocIndexes, DocIndexesBuilder};
 pub use self::shared_data::SharedData;
-
-unsafe fn into_u8_slice<T: Sized>(slice: &[T]) -> &[u8] {
-    let ptr = slice.as_ptr() as *const u8;
-    let len = slice.len() * size_of::<T>();
-    from_raw_parts(ptr, len)
-}
