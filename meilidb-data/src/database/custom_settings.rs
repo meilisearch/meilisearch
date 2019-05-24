a147c09b068a1948f0be17594d8d4c4df6d0ef73
Clément Renault <renault.cle@gmail.com>
2019-05-24 14:37:04 +0200

    feat: Make more functions accessible on the custom settings

--- meilidb-data/src/database/custom_settings.rs
+++ meilidb-data/src/database/custom_settings.rs
@@ -1,20 +1,13 @@
-use rocksdb::DBVector;
+use std::ops::Deref;
 use crate::database::raw_index::InnerRawIndex;
 
 #[derive(Clone)]
 pub struct CustomSettings(pub(crate) InnerRawIndex);
 
-impl CustomSettings {
-    pub fn set<K, V>(&self, key: K, value: V) -> Result<(), rocksdb::Error>
-    where K: AsRef<[u8]>,
-          V: AsRef<[u8]>,
-    {
-        self.0.set(key, value)
-    }
+impl Deref for CustomSettings {
+    type Target = InnerRawIndex;
 
-    pub fn get<K, V>(&self, key: K) -> Result<Option<DBVector>, rocksdb::Error>
-    where K: AsRef<[u8]>,
-    {
-        self.0.get(key)
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
