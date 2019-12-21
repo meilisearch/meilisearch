8c140f6bcdb141fec01f205136d7a3541178bae8
Clément Renault <renault.cle@gmail.com>
2019-12-22 00:37:22 +0100

    Increase the disk usage limit

--- meilisearch-core/src/database.rs
+++ meilisearch-core/src/database.rs
@@ -141,13 +141,13 @@ impl Database {
 
         fs::create_dir_all(&main_path)?;
         let env = heed::EnvOpenOptions::new()
-            .map_size(10 * 1024 * 1024 * 1024) // 10GB
+            .map_size(100 * 1024 * 1024 * 1024) // 100GB
             .max_dbs(3000)
             .open(main_path)?;
 
         fs::create_dir_all(&update_path)?;
         let update_env = heed::EnvOpenOptions::new()
-            .map_size(10 * 1024 * 1024 * 1024) // 10GB
+            .map_size(100 * 1024 * 1024 * 1024) // 100GB
             .max_dbs(3000)
             .open(update_path)?;
 
