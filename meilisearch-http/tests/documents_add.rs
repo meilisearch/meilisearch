69aee870da40bbcec3bfd6041dbaf868d85745c2
Clément Renault <renault.cle@gmail.com>
2020-03-31 10:39:58 +0200

    Make the engine index booleans
    The engine will see the values like text "true" and "false"

--- meilisearch-http/tests/documents_add.rs
+++ meilisearch-http/tests/documents_add.rs
@@ -36,3 +36,43 @@ fn check_add_documents_with_primary_key_param() {
     assert_eq!(status_code, 200);
     assert_eq!(response["status"], "processed");
 }
+
+// Test issue https://github.com/meilisearch/MeiliSearch/issues/568
+#[test]
+fn check_add_documents_with_nested_boolean() {
+    let mut server = common::Server::with_uid("tasks");
+
+    // 1 - Create the index with no primary_key
+
+    let body = json!({ "uid": "tasks" });
+    let (response, status_code) = server.create_index(body);
+    assert_eq!(status_code, 201);
+    assert_eq!(response["primaryKey"], json!(null));
+
+    // 2 - Add a document that contains a boolean in a nested object
+
+    let body = json!([{
+        "id": 12161,
+        "created_at": "2019-04-10T14:57:57.522Z",
+        "foo": {
+            "bar": {
+                "id": 121,
+                "crash": false
+            },
+            "id": 45912
+        }
+    }]);
+
+    let url = "/indexes/tasks/documents";
+    let (response, status_code) = server.post_request(&url, body);
+    eprintln!("{:#?}", response);
+    assert_eq!(status_code, 202);
+    let update_id = response["updateId"].as_u64().unwrap();
+    server.wait_update_id(update_id);
+
+    // 3 - Check update sucess
+
+    let (response, status_code) = server.get_update_status(update_id);
+    assert_eq!(status_code, 200);
+    assert_eq!(response["status"], "processed");
+}
