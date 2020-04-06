d24209f5a7d3f3dd6e3e8e127ccb6e1d972d709e
Clément Renault <renault.cle@gmail.com>
2020-04-06 19:25:56 +0200

    Adds a test to check that stop word ar correctly handled

--- meilisearch-http/tests/settings_stop_words.rs
+++ meilisearch-http/tests/settings_stop_words.rs
@@ -6,11 +6,7 @@ mod common;
 #[test]
 fn update_stop_words() {
     let mut server = common::Server::with_uid("movies");
-    let body = json!({
-        "uid": "movies",
-        "primaryKey": "id",
-    });
-    server.create_index(body);
+    server.populate_movies();
 
     // 1 - Get stop words
 
@@ -35,4 +31,33 @@ fn update_stop_words() {
 
     let (response, _status_code) = server.get_stop_words();
     assert_eq!(response.as_array().unwrap().is_empty(), true);
+}
+
+#[test]
+fn add_documents_and_stop_words() {
+    let mut server = common::Server::with_uid("movies");
+    server.populate_movies();
+
+    // 2 - Update stop words
+
+    let body = json!(["the", "of"]);
+    server.update_stop_words(body.clone());
+
+    // 3 - Search for a document with stop words
+
+    let (response, _status_code) = server.search("q=the%20mask");
+    assert!(!response["hits"].as_array().unwrap().is_empty());
+
+    // 4 - Search for documents with *only* stop words
+
+    let (response, _status_code) = server.search("q=the%20of");
+    assert!(response["hits"].as_array().unwrap().is_empty());
+
+    // 5 - Delete all stop words
+
+    // server.delete_stop_words();
+
+    // // 6 - Search for a document with one stop word
+
+    // assert!(!response["hits"].as_array().unwrap().is_empty());
 }
