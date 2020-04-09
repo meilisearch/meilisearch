a0a481697bc9e51b4e5868c41df9ccef9547ed4c
mposmta <postma.marin@protonmail.com>
2020-04-09 14:30:16 +0200

    replace lazy_static with once_cell

--- meilisearch-core/src/filters/parser/mod.rs
+++ meilisearch-core/src/filters/parser/mod.rs
@@ -1,13 +1,11 @@
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 use pest::prec_climber::{Operator, Assoc, PrecClimber};
 
-lazy_static! {
-    pub static ref PREC_CLIMBER: PrecClimber<Rule> = {
-        use Assoc::*;
-        use Rule::*;
-        pest::prec_climber::PrecClimber::new(vec![Operator::new(or, Left), Operator::new(and, Left)])
-    };
-}
+pub static PREC_CLIMBER: Lazy<PrecClimber<Rule>> = Lazy::new(|| {
+    use Assoc::*;
+    use Rule::*;
+    pest::prec_climber::PrecClimber::new(vec![Operator::new(or, Left), Operator::new(and, Left)])
+});
 
 #[derive(Parser)]
 #[grammar = "filters/parser/grammar.pest"]
