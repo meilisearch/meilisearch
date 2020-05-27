d9081fabed23b7939a9c42b960448c6d04480987
Kerollmops <clement@meilisearch.com>
2020-05-27 10:08:36 +0200

    Introduce the BitpackerSorted heed codec

--- meilisearch-core/src/heed_types/mod.rs
+++ meilisearch-core/src/heed_types/mod.rs
@@ -1,7 +1,9 @@
+mod bitpacker_sorted;
 mod cow_set;
 mod documents_ids;
 mod postings;
 
+pub use self::bitpacker_sorted::BitPackerSorted;
 pub use self::cow_set::CowSet;
 pub use self::documents_ids::{DocumentsIds, DiscoverIds};
 pub use self::postings::{Postings, PostingsCodec};
