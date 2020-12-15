5fe0e063426b5074067b511814a90eeaa9d3ed5e
mpostma <postma.marin@protonmail.com>
2020-12-15 12:42:19 +0100

    fix clippy warnings

--- meilisearch-core/src/update/documents_addition.rs
+++ meilisearch-core/src/update/documents_addition.rs
@@ -146,8 +146,8 @@ where A: AsRef<[u8]>,
     Ok(())
 }
 
-pub fn apply_addition<'a, 'b, 'c>(
-    writer: &'a mut heed::RwTxn<'b, 'c, MainT>,
+pub fn apply_addition(
+    writer: &mut heed::RwTxn<MainT>,
     index: &store::Index,
     new_documents: Vec<IndexMap<String, Value>>,
     partial: bool
@@ -272,16 +272,16 @@ pub fn apply_addition<'a, 'b, 'c>(
     Ok(())
 }
 
-pub fn apply_documents_partial_addition<'a, 'b, 'c>(
-    writer: &'a mut heed::RwTxn<'b, 'c, MainT>,
+pub fn apply_documents_partial_addition(
+    writer: &mut heed::RwTxn<MainT>,
     index: &store::Index,
     new_documents: Vec<IndexMap<String, Value>>,
 ) -> MResult<()> {
     apply_addition(writer, index, new_documents, true)
 }
 
-pub fn apply_documents_addition<'a, 'b, 'c>(
-    writer: &'a mut heed::RwTxn<'b, 'c, MainT>,
+pub fn apply_documents_addition(
+    writer: &mut heed::RwTxn<MainT>,
     index: &store::Index,
     new_documents: Vec<IndexMap<String, Value>>,
 ) -> MResult<()> {
