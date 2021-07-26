f79b8287f5bd43117e64ab6c04537b1c9d16755c
Tamo <tamo@meilisearch.com>
2021-07-26 15:25:30 +0200

    update vergen

--- meilisearch-http/build.rs
+++ meilisearch-http/build.rs
@@ -1,12 +1,7 @@
-use vergen::{generate_cargo_keys, ConstantsFlags};
+use vergen::{vergen, Config};
 
 fn main() {
-    // Setup the flags, toggling off the 'SEMVER_FROM_CARGO_PKG' flag
-    let mut flags = ConstantsFlags::all();
-    flags.toggle(ConstantsFlags::SEMVER_FROM_CARGO_PKG);
-
-    // Generate the 'cargo:' key output
-    generate_cargo_keys(ConstantsFlags::all()).expect("Unable to generate the cargo keys!");
+    vergen(Config::default()).unwrap();
 
     #[cfg(feature = "mini-dashboard")]
     mini_dashboard::setup_mini_dashboard().expect("Could not load the mini-dashboard assets");
