a260496c8ed1f39009c95cfaa65390fb16ec0227
mpostma <postma.marin@protonmail.com>
2021-10-13 11:24:51 +0200

    list updates

--- meilisearch-tasks/src/store.rs
+++ meilisearch-tasks/src/store.rs
@@ -1,18 +1,53 @@
+use std::borrow::Cow;
 use std::collections::{HashSet, VecDeque};
+use std::convert::TryInto;
 use std::sync::Arc;
 
 use chrono::Utc;
-use heed::types::{OwnedType, SerdeBincode};
-use heed::{Database, Env, RoTxn, RwTxn};
+use heed::types::{CowSlice, OwnedType, SerdeBincode, Str, Unit};
+use heed::{BytesDecode, BytesEncode, Database, Env, RoTxn, RwTxn};
 use tokio::sync::RwLock;
 
 use crate::task::{Task, TaskContent, TaskEvent, TaskId};
 use crate::Result;
 
+type TaskIdCodec = OwnedType<TaskId>;
+
+enum IndexUidTaskIdCodec {}
+
+impl<'a> BytesEncode<'a> for IndexUidTaskIdCodec {
+    type EItem = (&'a str, TaskId);
+
+    fn bytes_encode((s, id): &'a Self::EItem) -> Option<Cow<'a, [u8]>> {
+        let size = s.len() + std::mem::size_of::<TaskId>() + 1;
+        if size > 512 {
+            return None
+        }
+        let mut b = Vec::with_capacity(size);
+        b.extend_from_slice(s.as_bytes());
+        // null terminate the string
+        b.push(0);
+        b.extend_from_slice(&id.to_be_bytes());
+        Some(Cow::Owned(b))
+    }
+}
+
+impl<'a> BytesDecode<'a> for IndexUidTaskIdCodec {
+    type DItem = (&'a str, TaskId);
+
+    fn bytes_decode(bytes: &'a [u8]) -> Option<Self::DItem> {
+        let str_end = bytes.iter().position(|&it| it == 0)?;
+        let str_bytes = &bytes[..str_end];
+        let str = std::str::from_utf8(str_bytes).ok()?;
+        let id = TaskId::from_be_bytes(bytes[str_end + 1..].try_into().ok()?);
+        Some((str, id))
+    }
+}
+
 struct Store {
     env: Env,
-    // TODO: change to BE type
-    db: Database<OwnedType<TaskId>, SerdeBincode<Task>>,
+    uids_task_ids: Database<IndexUidTaskIdCodec, Unit>,
+    tasks: Database<TaskIdCodec, SerdeBincode<Task>>,
 }
 
 impl Store {
@@ -26,7 +61,7 @@ impl Store {
 
     fn next_task_id(&self, txn: &mut RwTxn) -> Result<TaskId> {
         let id = self
-            .db
+            .tasks
             .lazily_decode_data()
             .last(txn)?
             .map(|(id, _)| id)
@@ -35,14 +70,22 @@ impl Store {
     }
 
     fn put(&self, txn: &mut RwTxn, task: &Task) -> Result<()> {
-        let id = task.id;
-        self.db.put(txn, &id, &task)?;
+        self.tasks.put(txn, &task.id, task)?;
+        self.uids_task_ids.put(txn, &(&task.index_uid, task.id), &())?;
+
         Ok(())
     }
 
     fn get(&self, txn: &RoTxn, id: TaskId) -> Result<Option<Task>> {
-        let task = self.db.get(txn, &id)?;
+        let task = self.tasks.get(txn, &id)?;
         Ok(task)
+    }
+
+    fn list_updates<'a>(&self, txn: &'a RoTxn, from: Option<TaskId>) -> Result<Box<dyn Iterator<Item = heed::Result<Task>> + 'a>> {
+        match from {
+            Some(id) => Ok(Box::new(self.tasks.rev_range(txn, &(..id))?.map(|r| r.map(|(_, t)| t)))),
+            None => Ok(Box::new(self.tasks.rev_iter(txn)?.map(|r| r.map(|(_, t)| t)))),
+        }
     }
 }
 
@@ -116,5 +159,24 @@ impl TaskStore {
         pending_queue.retain(|id| !to_remove.contains(id));
 
         Ok(())
+    }
+
+    pub async fn list_updates(
+        &self,
+        filter: Option<impl Fn(&Task) -> bool + Send + Sync +  'static>,
+        limit: usize,
+        offset: Option<TaskId>,
+        ) -> Result<Vec<Task>> {
+        let store = self.store.clone();
+
+        tokio::task::spawn_blocking(move || {
+            let txn = store.rtxn()?;
+            let tasks = store.list_updates(&txn, offset)?
+                .filter_map(|t| t.ok())
+                .filter(|t| filter.as_ref().map(|f| f(t)).unwrap_or(true))
+                .take(limit)
+                .collect::<Vec<_>>();
+                Ok(tasks)
+        }).await?
     }
 }
