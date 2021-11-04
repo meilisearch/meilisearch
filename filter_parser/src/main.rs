b1a0110a47faf8362ee66513d121e45e1c28957d
Tamo <tamo@meilisearch.com>
2021-11-04 14:48:39 +0100

    update the main

--- filter_parser/src/main.rs
+++ filter_parser/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
     let input = std::env::args().nth(1).expect("You must provide a filter to test");
 
-    println!("Trying to execute the following filter:\n{}\n\n", input);
+    println!("Trying to execute the following filter:\n{}\n", input);
 
-    if let Err(e) = filter_parser::FilterCondition::parse(&input) {
-        println!("{}", e.to_string());
-    } else {
-        println!("✅ Valid filter");
+    match filter_parser::FilterCondition::parse(&input) {
+        Ok(filter) => {
+            println!("✅ Valid filter");
+            println!("{:#?}", filter);
+        }
+        Err(e) => {
+            println!("❎ Invalid filter");
+            println!("{}", e.to_string());
+        }
     }
 }
