39c42441d17369198b1866d7f7cd6e995eba79fd
Tamo <tamo@meilisearch.com>
2021-11-29 15:15:15 +0100

    Update meilisearch-lib/src/tasks/task.rs
    Co-authored-by: marin <postma.marin@protonmail.com>

--- meilisearch-lib/src/tasks/task.rs
+++ meilisearch-lib/src/tasks/task.rs
@@ -50,7 +50,7 @@ pub enum TaskEvent {
     },
 }
 
-/// A task represent an operation that Meilisearch must do.
+/// A task represents an operation that Meilisearch must do.
 /// It's stored on disk and executed from the lowest to higher Task id.
 /// Everytime a new task is created it has a higher Task id than the previous one.
 /// See also `Job`.
