629b897845445e5d57187674f5701dbd59a62455
Tamo <tamo@meilisearch.com>
2022-01-19 15:57:16 +0100

    feat(error): Update the error message when you have no version file

--- meilisearch-lib/src/index_controller/versioning/error.rs
+++ meilisearch-lib/src/index_controller/versioning/error.rs
@@ -1,6 +1,9 @@
 #[derive(thiserror::Error, Debug)]
 pub enum VersionFileError {
-    #[error("Version file is missing or the previous MeiliSearch engine version was below 0.24.0. Use a dump to update MeiliSearch.")]
+    #[error(
+        "MeilSearch (v{}) failed to infer the version of the database. Please consider using a dump to load your data.",
+        env!("CARGO_PKG_VERSION").to_string()
+    )]
     MissingVersionFile,
     #[error("Version file is corrupted and thus MeiliSearch is unable to determine the version of the database.")]
     MalformedVersionFile,
