b138b92d393e6d41928253290789bf1d464f9e27
Liu Hancheng <cn_lhc@qq.com>
2022-03-04 15:31:11 +0800

    show detailed error message

--- meilisearch-lib/src/document_formats.rs
+++ meilisearch-lib/src/document_formats.rs
@@ -34,12 +34,12 @@ impl Display for DocumentFormatError {
         match self {
             Self::Internal(e) => write!(f, "An internal error has occurred. `{}`.", e),
             Self::MalformedPayload(me, b) => match me.borrow() {
-                milli::documents::Error::JsonError(_) => write!(
+                milli::documents::Error::JsonError(se) => write!(
                     f,
-                    "The `{}` payload provided is malformed. line: {}",
-                    b, "TODO"
+                    "The `{}` payload provided is malformed. `Couldn't serialize document value. at line {} column {}`",
+                    b,se.line(),se.column()
                 ),
-                _ => write!(f, "The `{}` payload provided is malformed. line: {}", b, me),
+                _ => write!(f, "The `{}` payload provided is malformed. `{}`.", b, me),
             },
         }
     }
