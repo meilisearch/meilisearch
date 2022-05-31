1816db8c1f9328af92336ed05e25f1b1b78ab07d
ManyTheFish <many@meilisearch.com>
2022-05-31 15:39:09 +0200

    Move dump v4 patcher into v4.rs

--- meilisearch-auth/src/dump.rs
+++ meilisearch-auth/src/dump.rs
@@ -1,11 +1,9 @@
 use serde_json::Deserializer;
-use serde_json::{Map, Value};
-use std::fs;
+
 use std::fs::File;
 use std::io::BufReader;
 use std::io::Write;
 use std::path::Path;
-use uuid::Uuid;
 
 use crate::{AuthController, HeedAuthStore, Result};
 
@@ -42,32 +40,6 @@ impl AuthController {
         let reader = BufReader::new(File::open(&keys_file_path)?);
         for key in Deserializer::from_reader(reader).into_iter() {
             store.put_api_key(key?)?;
-        }
-
-        Ok(())
-    }
-
-    pub fn patch_dump_v4(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> Result<()> {
-        let keys_file_src = src.as_ref().join(KEYS_PATH);
-
-        if !keys_file_src.exists() {
-            return Ok(());
-        }
-
-        fs::create_dir_all(&dst)?;
-        let keys_file_dst = dst.as_ref().join(KEYS_PATH);
-        let mut writer = File::create(&keys_file_dst)?;
-
-        let reader = BufReader::new(File::open(&keys_file_src)?);
-        for key in Deserializer::from_reader(reader).into_iter() {
-            let mut key: Map<String, Value> = key?;
-
-            // generate a new uuid v4 and insert it in the key.
-            let uid = serde_json::to_value(Uuid::new_v4()).unwrap();
-            key.insert("uid".to_string(), uid);
-
-            serde_json::to_writer(&mut writer, &key)?;
-            writer.write_all(b"\n")?;
         }
 
         Ok(())
