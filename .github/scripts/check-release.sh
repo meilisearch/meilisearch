32c8846514dba7ea03b169c3002bf8365dd714ec
Clémentine Urquizar <clementine@meilisearch.com>
2022-06-22 12:20:12 +0200

    Rollback 0.0.0 versionning

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -3,24 +3,26 @@
 # check_tag $current_tag $file_tag $file_name
 function check_tag {
   if [ "$1" != "$2" ]; then
-      echo "Error: the current tag does not match the version in $3:"
-      echo "Found $1 - expected $2"
-      exit 1
+      echo "Error: the current tag does not match the version in $3: found $1 - expected $2"
+      ret=1
   fi
 }
 
+ret=0
 current_tag=$(echo $GITHUB_REF | tr -d 'refs/tags/v')
 
-files='*/Cargo.toml'
-for file in $files;
+toml_files='*/Cargo.toml'
+for toml_file in $toml_files;
 do
-    file_tag="$(grep '^version = ' $file | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')"
-    check_tag $current_tag $file_tag $file
+    file_tag="$(grep '^version = ' $toml_file | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')"
+    check_tag $current_tag $file_tag $toml_file
 done
 
 lock_file='Cargo.lock'
 lock_tag=$(grep -A 1 'name = "meilisearch-auth"' $lock_file | grep version | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 check_tag $current_tag $lock_tag $lock_file
 
-echo 'OK'
-exit 0
+if [ "$ret" -eq 0 ] ; then
+  echo 'OK'
+fi
+exit $ret
