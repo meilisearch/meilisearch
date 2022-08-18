993aa1321c34b872d2d10dc9c1ba65be54e54e68
ManyTheFish <many@meilisearch.com>
2022-08-18 17:56:06 +0200

    Fix query tree building

--- milli/src/search/query_tree.rs
+++ milli/src/search/query_tree.rs
@@ -454,7 +454,7 @@ fn create_query_tree(
 
     let mut operation_children = Vec::new();
     let mut query = query.to_vec();
-    for _ in 0..=remove_count {
+    for _ in 0..remove_count {
         let pos = match optional_words {
             TermsMatchingStrategy::All => return ngrams(ctx, authorize_typos, &query, false),
             TermsMatchingStrategy::Any => {
@@ -511,7 +511,7 @@ fn create_query_tree(
         };
     }
 
-    Ok(Operation::Or(true, operation_children))
+    Ok(Operation::or(true, operation_children))
 }
 
 /// Main function that matchings words used for crop and highlight.
