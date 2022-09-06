22d24dba56b3aa3f06014d6a7411d5a3b3f69139
Tamo <tamo@meilisearch.com>
2022-09-06 23:49:19 +0200

    implement the get_batch method

--- index-scheduler/src/error.rs
+++ index-scheduler/src/error.rs
@@ -1,7 +1,17 @@
+use milli::heed;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("Index not found")]
     IndexNotFound,
+    #[error("Corrupted task queue.")]
+    CorruptedTaskQueue,
+    #[error(transparent)]
+    Heed(#[from] heed::Error),
+    #[error(transparent)]
+    Milli(#[from] milli::Error),
+
+    #[error(transparent)]
+    Anyhow(#[from] anyhow::Error),
 }
