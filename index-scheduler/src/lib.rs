d7bb3b5a76f01e04ce1c92fdbca34ad4ceaf4697
Tamo <tamo@meilisearch.com>
2022-09-15 12:23:41 +0200

    test the register tasks

--- index-scheduler/src/lib.rs
+++ index-scheduler/src/lib.rs
@@ -15,3 +15,22 @@ pub use error::Error;
 pub use task::KindWithContent as TaskKind;
 /// from the exterior you don't need to know there is multiple type of `Task`
 pub use task::TaskView as Task;
+
+#[cfg(test)]
+mod tests {
+    #[macro_export]
+    macro_rules! assert_smol_debug_snapshot {
+    ($value:expr, @$snapshot:literal) => {{
+        let value = format!("{:?}", $value);
+        insta::assert_snapshot!(value, stringify!($value), @$snapshot);
+    }};
+    ($name:expr, $value:expr) => {{
+        let value = format!("{:?}", $value);
+        insta::assert_snapshot!(Some($name), value, stringify!($value));
+    }};
+    ($value:expr) => {{
+        let value = format!("{:?}", $value);
+        insta::assert_snapshot!($crate::_macro_support::AutoName, value, stringify!($value));
+    }};
+}
+}
