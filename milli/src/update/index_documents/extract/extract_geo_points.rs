00c02d00f344f86ef93d8fa264f1a60682ea7608
vishalsodani <vishalsodani@rediffmail.com>
2022-09-30 22:17:06 +0530

    Add missing logging timer to extractors

--- milli/src/update/index_documents/extract/extract_geo_points.rs
+++ milli/src/update/index_documents/extract/extract_geo_points.rs
@@ -12,6 +12,7 @@ use crate::{FieldId, InternalError, Result};
 /// Extracts the geographical coordinates contained in each document under the `_geo` field.
 ///
 /// Returns the generated grenad reader containing the docid as key associated to the (latitude, longitude)
+#[logging_timer::time]
 pub fn extract_geo_points<R: io::Read + io::Seek>(
     obkv_documents: grenad::Reader<R>,
     indexer: GrenadParameters,
