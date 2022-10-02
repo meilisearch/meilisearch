1c54bd1b9fb392826f6ce8a2011ea7b343cf02dd
Tamo <tamo@meilisearch.com>
2022-10-02 13:24:59 +0200

    move the DumpWriter and Error to their own module

---
+++ dump/src/error.rs
@@ -0,0 +1,9 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+}
