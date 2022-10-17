41bb8c0ab491748fd533dec5fdc4253abfc2727b
Kerollmops <clement@meilisearch.com>
2022-10-17 17:19:17 +0200

    Introduce the task cancelation task type

--- meilisearch-http/tests/dumps/mod.rs
+++ meilisearch-http/tests/dumps/mod.rs
@@ -23,7 +23,7 @@ async fn import_dump_v1() {
         };
         let error = Server::new_with_options(options)
             .await
-            .map(|_| ())
+            .map(drop)
             .unwrap_err();
 
         assert_eq!(error.to_string(), "The version 1 of the dumps is not supported anymore. You can re-export your dump from a version between 0.21 and 0.24, or start fresh from a version 0.25 onwards.");
