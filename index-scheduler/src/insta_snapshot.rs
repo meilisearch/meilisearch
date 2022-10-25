d667bee3ebd8eacc386ac70f16324d68cfe07e94
Kerollmops <clement@meilisearch.com>
2022-10-25 14:35:10 +0200

    Declare the auth path in the index scheduler

--- index-scheduler/src/insta_snapshot.rs
+++ index-scheduler/src/insta_snapshot.rs
@@ -27,6 +27,7 @@ pub fn snapshot_index_scheduler(scheduler: &IndexScheduler) -> String {
         wake_up: _,
         dumps_path: _,
         snapshots_path: _,
+        auth_path: _,
         test_breakpoint_sdr: _,
         planned_failures: _,
         run_loop_iteration: _,
