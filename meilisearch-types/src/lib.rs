c71a8ea183afa53312c8a91724ae93ff87124667
Loïc Lecrenier <loic.lecrenier@me.com>
2023-01-17 13:10:38 +0100

    Update to latest milli and deserr

--- meilisearch-types/src/lib.rs
+++ meilisearch-types/src/lib.rs
@@ -8,10 +8,11 @@ pub mod settings;
 pub mod star_or;
 pub mod tasks;
 pub mod versioning;
+pub use milli;
 pub use milli::{heed, Index};
+pub use serde_cs;
 use uuid::Uuid;
 pub use versioning::VERSION_FILE_NAME;
-pub use {milli, serde_cs};
 
 pub type Document = serde_json::Map<String, serde_json::Value>;
 pub type InstanceUid = Uuid;
