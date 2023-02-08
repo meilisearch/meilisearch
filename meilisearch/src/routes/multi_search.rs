56fd9637cd863141fb8536024ab105e5af8564b6
Louis Dureuil <louis@meilisearch.com>
2023-02-08 15:13:59 +0100

    Add basic analytics

--- meilisearch/src/routes/multi_search.rs
+++ meilisearch/src/routes/multi_search.rs
@@ -6,7 +6,7 @@ use meilisearch_types::deserr::DeserrJsonError;
 use meilisearch_types::error::ResponseError;
 use meilisearch_types::keys::actions;
 
-use crate::analytics::{Analytics, SearchAggregator};
+use crate::analytics::{Analytics, MultiSearchAggregator};
 use crate::extractors::authentication::policies::ActionPolicy;
 use crate::extractors::authentication::GuardedData;
 use crate::extractors::json::ValidatedJson;
@@ -26,29 +26,41 @@ pub async fn search_with_post(
     analytics: web::Data<dyn Analytics>,
 ) -> Result<HttpResponse, ResponseError> {
     let queries = params.into_inner();
-    let mut search_results = Vec::with_capacity(queries.len());
-    for (index_uid, mut query) in queries.into_iter().map(SearchQueryWithIndex::into_index_query) {
-        debug!("search called with params: {:?}", query);
 
-        // Tenant token search_rules.
-        if let Some(search_rules) =
-            index_scheduler.filters().search_rules.get_index_search_rules(&index_uid)
-        {
-            add_search_rules(&mut query, search_rules);
+    let mut multi_aggregate = MultiSearchAggregator::from_queries(&queries, &req);
+
+    let search_results: Result<_, ResponseError> = (|| {
+        async {
+            let mut search_results = Vec::with_capacity(queries.len());
+            for (index_uid, mut query) in
+                queries.into_iter().map(SearchQueryWithIndex::into_index_query)
+            {
+                debug!("search called with params: {:?}", query);
+
+                // Tenant token search_rules.
+                if let Some(search_rules) =
+                    index_scheduler.filters().search_rules.get_index_search_rules(&index_uid)
+                {
+                    add_search_rules(&mut query, search_rules);
+                }
+
+                let index = index_scheduler.index(&index_uid)?;
+                let search_result =
+                    tokio::task::spawn_blocking(move || perform_search(&index, query)).await?;
+
+                search_results.push(SearchResultWithIndex { index_uid, result: search_result? });
+            }
+            Ok(search_results)
         }
+    })()
+    .await;
 
-        let mut aggregate = SearchAggregator::from_query(&query, &req);
-
-        let index = index_scheduler.index(&index_uid)?;
-        let search_result =
-            tokio::task::spawn_blocking(move || perform_search(&index, query)).await?;
-        if let Ok(ref search_result) = search_result {
-            aggregate.succeed(search_result);
-        }
-        analytics.post_search(aggregate);
-
-        search_results.push(SearchResultWithIndex { index_uid, result: search_result? });
+    if search_results.is_ok() {
+        multi_aggregate.succeed();
     }
+    analytics.post_multi_search(multi_aggregate);
+
+    let search_results = search_results?;
 
     debug!("returns: {:?}", search_results);
 
