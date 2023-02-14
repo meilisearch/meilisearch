52b46e0d6c1977dabb602f0fd66a975bfcef1fc1
Louis Dureuil <louis@meilisearch.com>
2023-02-14 08:55:27 +0100

    Wrap queries and results in JSON objects

--- meilisearch/src/routes/multi_search.rs
+++ meilisearch/src/routes/multi_search.rs
@@ -5,6 +5,7 @@ use log::debug;
 use meilisearch_types::deserr::DeserrJsonError;
 use meilisearch_types::error::ResponseError;
 use meilisearch_types::keys::actions;
+use serde::Serialize;
 
 use crate::analytics::{Analytics, MultiSearchAggregator};
 use crate::extractors::authentication::policies::ActionPolicy;
@@ -19,9 +20,20 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("").route(web::post().to(SeqHandler(search_with_post))));
 }
 
+#[derive(Serialize)]
+struct SearchResults {
+    results: Vec<SearchResultWithIndex>,
+}
+
+#[derive(Debug, deserr::DeserializeFromValue)]
+#[deserr(error = DeserrJsonError, rename_all = camelCase, deny_unknown_fields)]
+pub struct SearchQueries {
+    queries: Vec<SearchQueryWithIndex>,
+}
+
 pub async fn search_with_post(
     index_scheduler: GuardedData<ActionPolicy<{ actions::SEARCH }>, Data<IndexScheduler>>,
-    params: ValidatedJson<Vec<SearchQueryWithIndex>, DeserrJsonError>,
+    params: ValidatedJson<SearchQueries, DeserrJsonError>,
     req: HttpRequest,
     analytics: web::Data<dyn Analytics>,
 ) -> Result<HttpResponse, ResponseError> {
@@ -64,5 +76,5 @@ pub async fn search_with_post(
 
     debug!("returns: {:?}", search_results);
 
-    Ok(HttpResponse::Ok().json(search_results))
+    Ok(HttpResponse::Ok().json(SearchResults { results: search_results }))
 }
