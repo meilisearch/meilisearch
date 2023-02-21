5cff435bf6e44bc2e727ccd1872167f64101a6d6
James <james.a.may.2001@gmail.com>
2023-02-21 07:41:41 +0000

    Add feature flags to Opt structure
    Resolves: #3469
    See also: #2763

--- meilisearch/src/analytics/segment_analytics.rs
+++ meilisearch/src/analytics/segment_analytics.rs
@@ -249,6 +249,7 @@ impl From<Opt> for Infos {
         // to add analytics when we add a field in the Opt.
         // Thus we must not insert `..` at the end.
         let Opt {
+            #[cfg(features = "metrics")]
             enable_metrics_route: _,
             db_path,
             http_addr,
