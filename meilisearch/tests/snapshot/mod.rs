e704728ee72f6a5a3ebf6e8bfeee08cf58d44153
Tamo <tamo@meilisearch.com>
2023-03-06 13:30:06 +0100

    fix the snapshots permissions on unix system

--- meilisearch/tests/snapshot/mod.rs
+++ meilisearch/tests/snapshot/mod.rs
@@ -1,8 +1,8 @@
 use std::time::Duration;
 
+use actix_rt::time::sleep;
 use meilisearch::option::ScheduleSnapshot;
 use meilisearch::Opt;
-use tokio::time::sleep;
 
 use crate::common::server::default_settings;
 use crate::common::{GetAllDocumentsOptions, Server};
@@ -23,14 +23,13 @@ macro_rules! verify_snapshot {
                     };
                     let (snapshot, _) = test(snapshot.clone()).await;
                     let (orig, _) = test(orig.clone()).await;
-                    assert_eq!(snapshot, orig);
+                    assert_eq!(snapshot, orig, "Got \n{}\nWhile expecting:\n{}", serde_json::to_string_pretty(&snapshot).unwrap(), serde_json::to_string_pretty(&orig).unwrap());
                 }
             )*
     };
 }
 
 #[actix_rt::test]
-#[ignore] // TODO: unignore
 async fn perform_snapshot() {
     let temp = tempfile::tempdir().unwrap();
     let snapshot_dir = tempfile::tempdir().unwrap();
@@ -56,11 +55,21 @@ async fn perform_snapshot() {
 
     index.wait_task(2).await;
 
-    sleep(Duration::from_secs(2)).await;
+    sleep(Duration::from_secs(1)).await;
 
     let temp = tempfile::tempdir().unwrap();
 
     let snapshot_path = snapshot_dir.path().to_owned().join("db.snapshot");
+    #[cfg_attr(windows, allow(unused))]
+    let snapshot_meta = std::fs::metadata(&snapshot_path).unwrap();
+
+    #[cfg(unix)]
+    {
+        use std::os::unix::fs::PermissionsExt;
+        let mode = snapshot_meta.permissions().mode();
+        //                                                    rwxrwxrwx
+        meili_snap::snapshot!(format!("{:b}", mode), @"1000000100100100");
+    }
 
     let options = Opt { import_snapshot: Some(snapshot_path), ..default_settings(temp.path()) };
 
@@ -71,7 +80,10 @@ async fn perform_snapshot() {
         // for some reason the db sizes differ. this may be due to the compaction options we have
         // set when performing the snapshot
         //server.stats(),
-        server.tasks(),
+
+        // We can't test all the tasks contained in the snapshot because the on the original instance the snapshotCreation task was added
+        server.tasks_filter("?from=4"),
+
         server.index("test").get_all_documents(GetAllDocumentsOptions::default()),
         server.index("test").settings(),
         server.index("test1").get_all_documents(GetAllDocumentsOptions::default()),
