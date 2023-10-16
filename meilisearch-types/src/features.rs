06678eeaf54e928bd03a7bd53a9df0c42f88d330
Vivek Kumar <vivek.26@outlook.com>
2023-10-16 16:10:46 +0530

    make metrics runtime togglable

--- meilisearch-types/src/features.rs
+++ meilisearch-types/src/features.rs
@@ -5,9 +5,19 @@ use serde::{Deserialize, Serialize};
 pub struct RuntimeTogglableFeatures {
     pub score_details: bool,
     pub vector_store: bool,
+    pub metrics: bool,
 }
 
 #[derive(Default, Debug, Clone, Copy)]
 pub struct InstanceTogglableFeatures {
     pub metrics: bool,
+}
+
+/// RuntimeToggledFeatures maintains the current value of the features that can be toggled at runtime.
+/// This is not a persistent structure, it is only used in memory.
+/// It is used for the features that are both togglable at runtime and instance-wide.
+/// We avoid database calls by storing the current value in memory.
+#[derive(Default, Debug, Clone, Copy)]
+pub struct RuntimeToggledFeatures {
+    pub metrics: (bool, bool), // (is_toggled, current_value)
 }
