a61d8c59ff198c6ad6081e5f584a90efb01be0b0
Louis Dureuil <louis@meilisearch.com>
2024-01-24 09:50:51 +0100

    Add span stats processor

---
+++ tracing-trace/src/bin/trace-to-callstats.rs
@@ -0,0 +1,16 @@
+use std::ffi::OsString;
+use std::io::Write;
+
+fn main() {
+    let input_file = std::env::args_os().nth(1).expect("missing <INPUT> file");
+    let input =
+        std::io::BufReader::new(std::fs::File::open(&input_file).expect("could not open <INPUT>"));
+    let trace = tracing_trace::TraceReader::new(input);
+    let profile = tracing_trace::processor::span_stats::to_call_stats(trace).unwrap();
+    let mut output_file = OsString::new();
+    output_file.push("callstats-");
+    output_file.push(input_file);
+    let mut output_file = std::io::BufWriter::new(std::fs::File::create(output_file).unwrap());
+    serde_json::to_writer(&mut output_file, &profile).unwrap();
+    output_file.flush().unwrap();
+}
