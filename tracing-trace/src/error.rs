ab9ecb28cc2e85946e1c2722b0a95ac3126e8a4e
Louis Dureuil <louis@meilisearch.com>
2024-01-31 17:44:28 +0100

    tracing-trace: implement Error on Error

--- tracing-trace/src/error.rs
+++ tracing-trace/src/error.rs
@@ -3,6 +3,8 @@ pub enum Error {
     Json(serde_json::Error),
 }
 
+impl std::error::Error for Error {}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str("error de/serializing trace entry:")?;
