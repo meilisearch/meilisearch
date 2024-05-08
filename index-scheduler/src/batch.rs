bba401eb3712fdf68b8c455c6d3585cede7bdb1f
Clément Renault <clement@meilisearch.com>
2024-05-08 15:26:21 +0200

    Prepare for processing documents edition

--- index-scheduler/src/batch.rs
+++ index-scheduler/src/batch.rs
@@ -103,6 +103,10 @@ pub(crate) enum IndexOperation {
         operations: Vec<DocumentOperation>,
         tasks: Vec<Task>,
     },
+    DocumentEdition {
+        index_uid: String,
+        task: Task,
+    },
     IndexDocumentDeletionByFilter {
         index_uid: String,
         task: Task,
@@ -161,7 +165,8 @@ impl Batch {
                 | IndexOperation::DocumentClear { tasks, .. } => {
                     RoaringBitmap::from_iter(tasks.iter().map(|task| task.uid))
                 }
-                IndexOperation::IndexDocumentDeletionByFilter { task, .. } => {
+                IndexOperation::DocumentEdition { task, .. }
+                | IndexOperation::IndexDocumentDeletionByFilter { task, .. } => {
                     RoaringBitmap::from_sorted_iter(std::iter::once(task.uid)).unwrap()
                 }
                 IndexOperation::SettingsAndDocumentOperation {
@@ -225,6 +230,7 @@ impl IndexOperation {
     pub fn index_uid(&self) -> &str {
         match self {
             IndexOperation::DocumentOperation { index_uid, .. }
+            | IndexOperation::DocumentEdition { index_uid, .. }
             | IndexOperation::IndexDocumentDeletionByFilter { index_uid, .. }
             | IndexOperation::DocumentClear { index_uid, .. }
             | IndexOperation::Settings { index_uid, .. }
@@ -239,6 +245,9 @@ impl fmt::Display for IndexOperation {
         match self {
             IndexOperation::DocumentOperation { .. } => {
                 f.write_str("IndexOperation::DocumentOperation")
+            }
+            IndexOperation::DocumentEdition { .. } => {
+                f.write_str("IndexOperation::DocumentEdition")
             }
             IndexOperation::IndexDocumentDeletionByFilter { .. } => {
                 f.write_str("IndexOperation::IndexDocumentDeletionByFilter")
@@ -283,6 +292,21 @@ impl IndexScheduler {
                     KindWithContent::DocumentDeletionByFilter { index_uid, .. } => {
                         Ok(Some(Batch::IndexOperation {
                             op: IndexOperation::IndexDocumentDeletionByFilter {
+                                index_uid: index_uid.clone(),
+                                task,
+                            },
+                            must_create_index: false,
+                        }))
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            BatchKind::DocumentEdition { id } => {
+                let task = self.get_task(rtxn, id)?.ok_or(Error::CorruptedTaskQueue)?;
+                match &task.kind {
+                    KindWithContent::DocumentEdition { index_uid, .. } => {
+                        Ok(Some(Batch::IndexOperation {
+                            op: IndexOperation::DocumentEdition {
                                 index_uid: index_uid.clone(),
                                 task,
                             },
@@ -1333,6 +1357,9 @@ impl IndexScheduler {
                 }
 
                 Ok(tasks)
+            }
+            IndexOperation::DocumentEdition { .. } => {
+                todo!()
             }
             IndexOperation::IndexDocumentDeletionByFilter { mut task, index_uid: _ } => {
                 let filter =
