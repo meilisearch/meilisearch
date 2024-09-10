c1c44a0b81c4ee422141f2e67034371565943d22
Clément Renault <clement@meilisearch.com>
2024-09-10 19:32:03 +0100

    Impl serialize on TopLevelMap

--- milli/src/update/new/indexer/top_level_map.rs
+++ milli/src/update/new/indexer/top_level_map.rs
@@ -2,13 +2,13 @@ use std::borrow::{Borrow, Cow};
 use std::collections::BTreeMap;
 use std::fmt;
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::value::RawValue;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct TopLevelMap<'p>(#[serde(borrow)] pub BTreeMap<CowStr<'p>, &'p RawValue>);
 
-#[derive(Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
+#[derive(Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
 pub struct CowStr<'p>(#[serde(borrow)] pub Cow<'p, str>);
 
 impl fmt::Display for CowStr<'_> {
