01bcc601beb72f0011568e164f459309530185f6
Kerollmops <clement@meilisearch.com>
2024-12-11 12:04:29 +0100

    Use a nonrandom hasher when decoding JSON

--- crates/meilisearch-types/src/document_formats.rs
+++ crates/meilisearch-types/src/document_formats.rs
@@ -221,7 +221,7 @@ pub fn read_json(input: &File, output: impl io::Write) -> Result<u64> {
     let mut deserializer = serde_json::Deserializer::from_slice(&input);
     let res = array_each(&mut deserializer, |obj: &RawValue| {
         doc_alloc.reset();
-        let map = RawMap::from_raw_value(obj, &doc_alloc)?;
+        let map = RawMap::from_raw_value_and_hasher(obj, FxBuildHasher, &doc_alloc)?;
         to_writer(&mut out, &map)
     });
     let count = match res {
