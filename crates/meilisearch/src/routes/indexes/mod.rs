668b26b6415417f0ba76b42896538b5814c136e3
Tamo <tamo@meilisearch.com>
2024-12-23 16:11:22 +0100

    add the facet search

--- crates/meilisearch/src/routes/indexes/mod.rs
+++ crates/meilisearch/src/routes/indexes/mod.rs
@@ -41,6 +41,7 @@ mod similar_analytics;
 #[openapi(
     nest(
         (path = "/", api = documents::DocumentsApi),
+        (path = "/", api = facet_search::FacetSearchApi),
     ),
     paths(list_indexes, create_index, get_index, update_index, delete_index, get_index_stats),
     tags(
