445e5aff02c8d16f8029584e4b18e8d7d3fe83b5
Tamo <tamo@meilisearch.com>
2025-01-15 12:38:40 +0100

    fix the corruption

--- crates/index-scheduler/src/scheduler/test.rs
+++ crates/index-scheduler/src/scheduler/test.rs
@@ -897,7 +897,25 @@ fn create_and_list_index() {
     // but the indexUpdate task has not been processed yet
     handle.advance_till([InsideProcessBatch]);
 
-    index_scheduler.index("kefir").unwrap(); // Crash on corrupted task queue
-    let empty = index_scheduler.get_paginated_indexes_stats(&AuthFilter::default(), 0, 20).unwrap();
-    snapshot!(format!("{empty:?}"), @"");
+    index_scheduler.index("kefir").unwrap();
+    let list = index_scheduler.get_paginated_indexes_stats(&AuthFilter::default(), 0, 20).unwrap();
+    snapshot!(json_string!(list, { "[1][0][1].created_at" => "[date]", "[1][0][1].updated_at" => "[date]" }), @r#"
+    [
+      1,
+      [
+        [
+          "kefir",
+          {
+            "number_of_documents": 0,
+            "database_size": 24576,
+            "used_database_size": 8192,
+            "primary_key": null,
+            "field_distribution": {},
+            "created_at": "[date]",
+            "updated_at": "[date]"
+          }
+        ]
+      ]
+    ]
+    "#);
 }
