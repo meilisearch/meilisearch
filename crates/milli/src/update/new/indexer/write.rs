a6470a0c37821535a4799f7421b54e8862225c9a
Louis Dureuil <louis@meilisearch.com>
2025-01-16 14:33:53 +0100

    Improve error log

--- crates/milli/src/update/new/indexer/write.rs
+++ crates/milli/src/update/new/indexer/write.rs
@@ -155,7 +155,8 @@ pub fn write_from_bbqueue(
                         Ok(false) => {
                             tracing::error!(
                                 database_name,
-                                key = %key.as_bstr(),
+                                key_bytes = ?key,
+                                formatted_key = ?key.as_bstr(),
                                 "Attempt to delete an unknown key"
                             );
                         }
