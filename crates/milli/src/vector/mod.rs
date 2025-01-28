6a1062edf54a9ccaf4dfdb3501cf9a1e9c482db5
Kerollmops <clement@meilisearch.com>
2025-01-28 16:20:33 +0100

    Add more logs to see calls to the embedders

--- crates/milli/src/vector/mod.rs
+++ crates/milli/src/vector/mod.rs
@@ -637,6 +637,7 @@ impl Embedder {
         }
     }
 
+    #[tracing::instrument(level = "debug", skip_all, target = "indexing::vector")]
     pub fn embed_chunks_ref(
         &self,
         texts: &[&str],
