48812229a9e8d4ece7330b37c6de24a5a7037c2a
Kerollmops <clement@meilisearch.com>
2025-01-28 17:41:37 +0100

    Remove a log that would log too much

--- crates/milli/src/vector/mod.rs
+++ crates/milli/src/vector/mod.rs
@@ -638,7 +638,6 @@ impl Embedder {
         }
     }
 
-    #[tracing::instrument(level = "debug", skip_all, target = "indexing::vector")]
     pub fn embed_chunks_ref(
         &self,
         texts: &[&str],
