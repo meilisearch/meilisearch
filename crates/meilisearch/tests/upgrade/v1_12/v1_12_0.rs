077a3a2cb343eb0b607e713ab62f371b88449e38
ManyTheFish <many@meilisearch.com>
2025-02-11 10:01:22 +0100

    fix snapshots

--- crates/meilisearch/tests/upgrade/v1_12/v1_12_0.rs
+++ crates/meilisearch/tests/upgrade/v1_12/v1_12_0.rs
@@ -134,6 +134,9 @@ async fn check_the_index_scheduler(server: &Server) {
       "indexes": {
         "kefir": {
           "numberOfDocuments": 1,
+          "rawDocumentDbSize": 109,
+          "maxDocumentSize": 109,
+          "avgDocumentSize": 109,
           "isIndexing": false,
           "fieldDistribution": {
             "age": 1,
@@ -214,6 +217,9 @@ async fn check_the_index_scheduler(server: &Server) {
       "indexes": {
         "kefir": {
           "numberOfDocuments": 1,
+          "rawDocumentDbSize": 109,
+          "maxDocumentSize": 109,
+          "avgDocumentSize": 109,
           "isIndexing": false,
           "fieldDistribution": {
             "age": 1,
@@ -228,9 +234,12 @@ async fn check_the_index_scheduler(server: &Server) {
     "###);
     let index = server.index("kefir");
     let (stats, _) = index.stats().await;
-    snapshot!(stats, @r#"
+    snapshot!(stats, @r###"
     {
       "numberOfDocuments": 1,
+      "rawDocumentDbSize": 109,
+      "maxDocumentSize": 109,
+      "avgDocumentSize": 109,
       "isIndexing": false,
       "fieldDistribution": {
         "age": 1,
@@ -240,7 +249,7 @@ async fn check_the_index_scheduler(server: &Server) {
         "surname": 1
       }
     }
-    "#);
+    "###);
 
     // Delete all the tasks of a specific batch
     let (task, _) = server.delete_tasks("batchUids=10").await;
