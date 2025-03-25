43c8a206b43c61712e4a5a7861b82eecc2fc6ddb
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-03-25 13:07:17 +0100

    detail comments

--- crates/milli/src/update/new/indexer/write.rs
+++ crates/milli/src/update/new/indexer/write.rs
@@ -218,7 +218,7 @@ pub fn write_from_bbqueue(
                     arroy_writers.get(&embedder_id).expect("requested a missing embedder");
                 let mut embeddings = Embeddings::new(*dimensions);
                 let all_embeddings = asvs.read_all_embeddings_into_vec(frame, aligned_embedding);
-                // FIXME: /!\ Case where #embeddings is divisor of `dimensions` would still pass
+                // FIXME: /!\ Case where number of embeddings is divisor of `dimensions` would still pass
                 if *dimensions!= 0 && all_embeddings.len() % *dimensions != 0 {
                     return Err(Error::UserError(UserError::InvalidVectorDimensions {
                         expected: *dimensions,
