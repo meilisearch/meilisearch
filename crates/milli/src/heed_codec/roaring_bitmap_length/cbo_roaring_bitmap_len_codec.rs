05e0cab0aba1221658627cfe85654ba837ad233d
Kerollmops <clement@meilisearch.com>
2025-06-17 14:45:47 +0200

    Effectively remove CBOs

--- crates/milli/src/heed_codec/roaring_bitmap_length/cbo_roaring_bitmap_len_codec.rs
+++ crates/milli/src/heed_codec/roaring_bitmap_length/cbo_roaring_bitmap_len_codec.rs
@@ -1,9 +1,6 @@
-use std::mem;
-
 use heed::{BoxedError, BytesDecode};
 
-use super::{BoRoaringBitmapLenCodec, RoaringBitmapLenCodec};
-use crate::heed_codec::roaring_bitmap::cbo_roaring_bitmap_codec::THRESHOLD;
+use super::RoaringBitmapLenCodec;
 use crate::heed_codec::BytesDecodeOwned;
 
 pub struct CboRoaringBitmapLenCodec;
@@ -12,15 +9,9 @@ impl BytesDecode<'_> for CboRoaringBitmapLenCodec {
     type DItem = u64;
 
     fn bytes_decode(bytes: &[u8]) -> Result<Self::DItem, BoxedError> {
-        if bytes.len() <= THRESHOLD * mem::size_of::<u32>() {
-            // If there is threshold or less than threshold integers that can fit into this array
-            // of bytes it means that we used the ByteOrder codec serializer.
-            BoRoaringBitmapLenCodec::bytes_decode(bytes)
-        } else {
-            // Otherwise, it means we used the classic RoaringBitmapCodec and
-            // that the header takes threshold integers.
-            RoaringBitmapLenCodec::bytes_decode(bytes)
-        }
+        // Otherwise, it means we used the classic RoaringBitmapCodec and
+        // that the header takes threshold integers.
+        RoaringBitmapLenCodec::bytes_decode(bytes)
     }
 }
 
