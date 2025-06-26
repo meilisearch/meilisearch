9422b6d654cf44a60d540444310e9c96b173b18a
Mubelotix <mubelotix@gmail.com>
2025-06-26 10:58:27 +0200

    Update crates/meilisearch/src/lib.rs
    Co-authored-by: Louis Dureuil <louis.dureuil@gmail.com>

--- crates/meilisearch/src/lib.rs
+++ crates/meilisearch/src/lib.rs
@@ -543,7 +543,7 @@ fn import_dump(
         tracing::info!("Importing the settings.");
         let settings = index_reader.settings()?;
         apply_settings_to_builder(&settings, &mut builder);
-        let embedder_stats: Arc<EmbedderStats> = Default::default(); // FIXME: this isn't linked to anything
+        let embedder_stats: Arc<EmbedderStats> = Default::default();
         builder.execute(
             |indexing_step| tracing::debug!("update: {:?}", indexing_step),
             || false,
