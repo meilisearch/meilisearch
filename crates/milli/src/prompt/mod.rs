d2e4d6dd8ae78273fe7644262fbdf86116273276
Louis Dureuil <louis@meilisearch.com>
2025-06-30 00:11:40 +0200

    prompt: Publishes some types

--- crates/milli/src/prompt/mod.rs
+++ crates/milli/src/prompt/mod.rs
@@ -9,12 +9,11 @@ use std::fmt::Debug;
 use std::num::NonZeroUsize;
 
 use bumpalo::Bump;
-use document::ParseableDocument;
+pub(crate) use document::{Document, ParseableDocument};
 use error::{NewPromptError, RenderPromptError};
-use fields::{BorrowedFields, OwnedFields};
+pub use fields::{BorrowedFields, OwnedFields};
 
-use self::context::Context;
-use self::document::Document;
+pub use self::context::Context;
 use crate::fields_ids_map::metadata::FieldIdMapWithMetadata;
 use crate::update::del_add::DelAdd;
 use crate::GlobalFieldsIdsMap;
