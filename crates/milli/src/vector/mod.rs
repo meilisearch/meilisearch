f244439b4f7493edb4a7f2387f80ae932cd3229b
Mubelotix <simon@meilisearch.com>
2025-07-10 16:43:45 +0200

    Revert "Format"
    This reverts commit 30fd546c1227bbfc3e71e3541f41847fca4c4713.

--- crates/milli/src/vector/mod.rs
+++ crates/milli/src/vector/mod.rs
@@ -485,8 +485,6 @@ impl ArroyWrapper {
         limit: usize,
         filter: Option<&RoaringBitmap>,
     ) -> Result<Vec<(ItemId, f32)>, arroy::Error> {
-        println!("nns_by_vector: quantized={} {:?} limit={} filter={:?}",
-            self.quantized, vector, limit, filter);
         if self.quantized {
             self._nns_by_vector(rtxn, self.quantized_db(), vector, limit, filter)
         } else {
@@ -518,8 +516,6 @@ impl ArroyWrapper {
         }
 
         results.sort_unstable_by_key(|(_, distance)| OrderedFloat(*distance));
-
-        println!("nns_by_vector: results={:?}", results);
 
         Ok(results)
     }
