54b85b86445a9060e52db7d1bdd1ee794f3c6614
Tamo <tamo@meilisearch.com>
2025-08-11 16:37:09 +0200

    fix the dumpless upgrade again

--- crates/index-scheduler/src/upgrade/mod.rs
+++ crates/index-scheduler/src/upgrade/mod.rs
@@ -39,6 +39,7 @@ pub fn upgrade_index_scheduler(
         (1, 13, _) => 0,
         (1, 14, _) => 0,
         (1, 15, _) => 0,
+        (1, 16, _) => 0,
         (major, minor, patch) => {
             if major > current_major
                 || (major == current_major && minor > current_minor)
