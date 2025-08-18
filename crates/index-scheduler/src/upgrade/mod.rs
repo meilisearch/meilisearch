d68e6c355bbfa6ce6bcfacce0f4738a7a8ec3e63
Louis Dureuil <louis@meilisearch.com>
2025-08-18 16:15:20 +0200

    update to v1.22

--- crates/index-scheduler/src/upgrade/mod.rs
+++ crates/index-scheduler/src/upgrade/mod.rs
@@ -42,6 +42,7 @@ pub fn upgrade_index_scheduler(
         (1, 16, _) => 0,
         (1, 17, _) => 0,
         (1, 18, _) => 0,
+        (1, 22, _) => 0,
         (major, minor, patch) => {
             if major > current_major
                 || (major == current_major && minor > current_minor)
