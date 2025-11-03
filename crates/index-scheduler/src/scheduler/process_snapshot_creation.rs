dc456276e5e0d795f1d85bfef659344c595bb69b
Kerollmops <clement@meilisearch.com>
2025-11-03 17:45:29 +0100

    Make clippy happy

--- crates/index-scheduler/src/scheduler/process_snapshot_creation.rs
+++ crates/index-scheduler/src/scheduler/process_snapshot_creation.rs
@@ -280,6 +280,7 @@ impl IndexScheduler {
     }
 
     #[cfg(unix)]
+    #[allow(clippy::too_many_arguments)]
     pub(super) async fn process_snapshot_to_s3(
         &self,
         progress: Progress,
