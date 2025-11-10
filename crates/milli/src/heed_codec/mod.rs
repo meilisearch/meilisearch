6f3251810351ac956b045d327dddae38f7bae2ff
Kerollmops <clement@meilisearch.com>
2025-11-10 16:56:19 +0100

    Introduce a first working version of the DeBitmapCodec

--- crates/milli/src/heed_codec/mod.rs
+++ crates/milli/src/heed_codec/mod.rs
@@ -1,6 +1,7 @@
 mod beu16_str_codec;
 mod beu32_str_codec;
 mod byte_slice_ref;
+mod de_bitmap;
 pub mod facet;
 mod field_id_word_count_codec;
 mod fst_set_codec;
@@ -19,6 +20,7 @@ use thiserror::Error;
 
 pub use self::beu16_str_codec::BEU16StrCodec;
 pub use self::beu32_str_codec::BEU32StrCodec;
+pub use self::de_bitmap::DeBitmapCodec;
 pub use self::field_id_word_count_codec::FieldIdWordCountCodec;
 pub use self::fst_set_codec::FstSetCodec;
 pub use self::obkv_codec::ObkvCodec;
