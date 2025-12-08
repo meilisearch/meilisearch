d6cd954e4ba273ef64d96a6ef41e514d651c0298
Louis Dureuil <louis@meilisearch.com>
2025-12-08 10:54:45 +0100

    Update shard definition

--- crates/milli/src/update/new/indexer/enterprise_edition/sharding.rs
+++ crates/milli/src/update/new/indexer/enterprise_edition/sharding.rs
@@ -5,18 +5,38 @@
 
 use std::hash::{BuildHasher as _, BuildHasherDefault};
 
-pub struct Shards {
-    pub own: Vec<String>,
-    pub others: Vec<String>,
+#[derive(Debug, Clone)]
+pub struct Shards(pub Vec<Shard>);
+
+#[derive(Debug, Clone)]
+pub struct Shard {
+    pub is_own: bool,
+    pub name: String,
 }
 
 impl Shards {
+    pub fn from_remotes_local<'a>(
+        remotes: impl IntoIterator<Item = &'a str>,
+        local: Option<&str>,
+    ) -> Self {
+        Shards(
+            remotes
+                .into_iter()
+                .map(|name| Shard { is_own: Some(name) == local, name: name.to_owned() })
+                .collect(),
+        )
+    }
+
     pub fn must_process(&self, docid: &str) -> bool {
+        self.processing_shard(docid).map(|shard| shard.is_own).unwrap_or_default()
+    }
+
+    pub fn processing_shard<'a>(&'a self, docid: &str) -> Option<&'a Shard> {
         let hasher = BuildHasherDefault::<twox_hash::XxHash3_64>::new();
-        let to_hash = |shard: &String| hasher.hash_one((shard, docid));
+        let to_hash = |shard: &'a Shard| (shard, hasher.hash_one((&shard.name, docid)));
 
-        let max_hash = self.others.iter().map(to_hash).max().unwrap_or_default();
-
-        self.own.iter().map(to_hash).any(|hash| hash > max_hash)
+        let shard =
+            self.0.iter().map(to_hash).max_by_key(|(_, hash)| *hash).map(|(shard, _)| shard);
+        shard
     }
 }
