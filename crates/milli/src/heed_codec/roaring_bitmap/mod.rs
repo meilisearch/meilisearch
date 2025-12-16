4a97d60c605cc4851b97e4b4e8a0be1ad2c73a3e
Kerollmops <clement@meilisearch.com>
2025-12-16 17:04:11 +0100

    Implement merge_deladd_into on DeCboRoaringBitmapCodec

--- crates/milli/src/heed_codec/roaring_bitmap/mod.rs
+++ crates/milli/src/heed_codec/roaring_bitmap/mod.rs
@@ -5,6 +5,7 @@ mod de_roaring_bitmap_codec;
 mod roaring_bitmap_codec;
 
 pub use self::bo_roaring_bitmap_codec::BoRoaringBitmapCodec;
-// pub use self::cbo_roaring_bitmap_codec::CboRoaringBitmapCodec;
+pub use self::cbo_roaring_bitmap_codec::CboRoaringBitmapCodec;
 pub use self::de_cbo_roaring_bitmap_codec::{DeCboRoaringBitmapCodec, DELTA_ENCODING_STATUS};
+pub use self::de_roaring_bitmap_codec::{take_all_blocks, DeRoaringBitmapCodec};
 pub use self::roaring_bitmap_codec::RoaringBitmapCodec;
