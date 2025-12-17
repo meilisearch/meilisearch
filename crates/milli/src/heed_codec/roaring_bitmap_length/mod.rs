9c6f9729b8fa56f3d7e293beb2adfef0952c2d67
Kerollmops <clement@meilisearch.com>
2025-12-17 09:27:39 +0100

    WIP

--- crates/milli/src/heed_codec/roaring_bitmap_length/mod.rs
+++ crates/milli/src/heed_codec/roaring_bitmap_length/mod.rs
@@ -3,7 +3,7 @@ mod cbo_roaring_bitmap_len_codec;
 mod de_cbo_roaring_bitmap_len_codec;
 mod roaring_bitmap_len_codec;
 
-pub use self::bo_roaring_bitmap_len_codec::BoRoaringBitmapLenCodec;
+use self::bo_roaring_bitmap_len_codec::BoRoaringBitmapLenCodec;
 use self::cbo_roaring_bitmap_len_codec::CboRoaringBitmapLenCodec;
 pub use self::de_cbo_roaring_bitmap_len_codec::DeCboRoaringBitmapLenCodec;
-pub use self::roaring_bitmap_len_codec::RoaringBitmapLenCodec;
+use self::roaring_bitmap_len_codec::RoaringBitmapLenCodec;
