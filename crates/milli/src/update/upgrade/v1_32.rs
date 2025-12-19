15d3a6a43979246ce86c18a87050b17de81280f6
Kerollmops <clement@meilisearch.com>
2025-12-19 12:14:05 +0100

    Use an optimized version to rebuild all graph links

--- crates/milli/src/update/upgrade/v1_32.rs
+++ crates/milli/src/update/upgrade/v1_32.rs
@@ -4,12 +4,19 @@ use heed::RwTxn;
 
 use super::UpgradeIndex;
 use crate::progress::Progress;
-use crate::{make_enum_progress, Index, Result};
+use crate::vector::VectorStore;
+use crate::{make_enum_progress, Index, MustStopProcessing, Result};
 
 pub(super) struct CleanupFidBasedDatabases();
 
 impl UpgradeIndex for CleanupFidBasedDatabases {
-    fn upgrade(&self, wtxn: &mut RwTxn, index: &Index, progress: Progress) -> Result<bool> {
+    fn upgrade(
+        &self,
+        wtxn: &mut RwTxn,
+        index: &Index,
+        must_stop_processing: &MustStopProcessing,
+        progress: Progress,
+    ) -> Result<bool> {
         make_enum_progress! {
             enum CleanupFidBasedDatabases {
                 RetrievingFidsToDelete,
@@ -31,7 +38,7 @@ impl UpgradeIndex for CleanupFidBasedDatabases {
             crate::update::new::indexer::delete_old_fid_based_databases_from_fids(
                 wtxn,
                 index,
-                &|| false,
+                &|| must_stop_processing.get(),
                 &fids_to_delete,
                 &progress,
             )?;
@@ -45,5 +52,54 @@ impl UpgradeIndex for CleanupFidBasedDatabases {
 
     fn description(&self) -> &'static str {
         "Cleaning up the fid-based databases"
+    }
+}
+
+/// Rebuilds the hannoy graph and do not touch to the embeddings.
+///
+/// This follows a bug in hannoy v0.0.9 and v0.1.0 where the graph
+/// was not built correctly.
+pub(super) struct RebuildHannoyGraph();
+
+impl UpgradeIndex for RebuildHannoyGraph {
+    fn upgrade(
+        &self,
+        wtxn: &mut RwTxn,
+        index: &Index,
+        must_stop_processing: &MustStopProcessing,
+        progress: Progress,
+    ) -> Result<bool> {
+        let embedders = index.embedding_configs();
+        let backend = index.get_vector_store(wtxn)?.unwrap_or_default();
+
+        for config in embedders.embedding_configs(wtxn)? {
+            let embedder_info = embedders.embedder_info(wtxn, &config.name)?.unwrap();
+            let mut vector_store = VectorStore::new(
+                backend,
+                index.vector_store,
+                embedder_info.embedder_id,
+                config.config.quantized(),
+            );
+
+            let seed = rand::random();
+            let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
+            vector_store.rebuild_graph(
+                wtxn,
+                progress.clone(),
+                &mut rng,
+                vector_store.dimensions(wtxn)?.unwrap(),
+                &|| must_stop_processing.get(),
+            )?;
+        }
+
+        Ok(false)
+    }
+
+    fn must_upgrade(&self, initial_version: (u32, u32, u32)) -> bool {
+        initial_version < (1, 32, 0)
+    }
+
+    fn description(&self) -> &'static str {
+        "Rebuilding graph links"
     }
 }
