613220d8d0682c735768e91bf42dafced0665d0b
Kerollmops <clement@meilisearch.com>
2026-01-12 12:07:51 +0100

    Simplify the safe_unpack code

--- crates/meilisearch-types/src/archive_ext.rs
+++ crates/meilisearch-types/src/archive_ext.rs
@@ -1,5 +1,5 @@
 use std::fs::DirEntry;
-use std::path::{self, Path};
+use std::path::Path;
 use std::{fs, io};
 
 use tar::Archive;
@@ -11,62 +11,14 @@ pub trait ArchiveExt {
 }
 
 impl<R: io::Read> ArchiveExt for Archive<R> {
-    /// Most of the dcode comes from the `tar` crate. The destination path must be absolute.
+    /// Most of the dcode comes from the `tar` crate.
     ///
     /// <https://github.com/alexcrichton/tar-rs/blob/20a650970793e56238b58ac2f51773d343b02117/src/archive.rs#L217-L257>
     fn safe_unpack(&mut self, dst: impl AsRef<Path>) -> io::Result<()> {
-        // Note that I should create a subfunction with non-generic types
-        // but the `Archive::entries` method doesn't work on non-sized types...
         let dst = dst.as_ref();
 
-        if dst.symlink_metadata().is_err() {
-            fs::create_dir_all(dst).map_err(|e| {
-                io::Error::new(e.kind(), format!("failed to create `{}`", dst.display()))
-            })?;
-        }
-
-        // Canonicalizing the dst directory will prepend the path with '\\?\'
-        // on windows which will allow windows APIs to treat the path as an
-        // extended-length path with a 32,767 character limit. Otherwise all
-        // unpacked paths over 260 characters will fail on creation with a
-        // NotFound exception.
-        let dst = &dst.canonicalize().unwrap_or(dst.to_path_buf());
-
-        // Delay any directory entries until the end (they will be created if needed by
-        // descendants), to ensure that directory permissions do not interfer with descendant
-        // extraction.
-        let mut directories = Vec::new();
-        for entry in self.entries()? {
-            let mut entry =
-                entry.map_err(|e| io::Error::new(e.kind(), "failed to iterate over archive"))?;
-            match entry.header().entry_type() {
-                tar::EntryType::Directory => directories.push(entry),
-                _ => {
-                    if let Some(link_name) = entry.header().link_name()? {
-                        // The destination path must be absolute.
-                        let absolute_link_name = path::absolute(dst.join(&link_name))?;
-                        if !absolute_link_name.starts_with(dst) {
-                            return Err(io::Error::other(
-                                "links and symlinks must link within the dump directory",
-                            ));
-                        }
-                    }
-                    entry.unpack_in(dst)?;
-                }
-            }
-        }
-
-        // Apply the directories.
-        //
-        // Note: the order of application is important to permissions. That is, we must traverse
-        // the filesystem graph in topological ordering or else we risk not being able to create
-        // child directories within those of more restrictive permissions. See [0] for details.
-        //
-        // [0]: <https://github.com/alexcrichton/tar-rs/issues/242>
-        directories.sort_by(|a, b| b.path_bytes().cmp(&a.path_bytes()));
-        for mut dir in directories {
-            dir.unpack_in(dst)?;
-        }
+        // Unpack the archive into the destination directory.
+        self.unpack(dst)?;
 
         // Finally check the unpacked files and directories
         // to check if symlinks are pointing inside the dst folder.
