f0ee67c28d3da6b08d5093a8bdd5a11724332b40
Louis Dureuil <louis@meilisearch.com>
2026-01-13 09:05:17 +0100

    Fix async-openai tests

--- external-crates/async-openai/src/lib.rs
+++ external-crates/async-openai/src/lib.rs
@@ -6,11 +6,11 @@
 //! use async_openai::{Client, config::OpenAIConfig};
 //!
 //! // Create a OpenAI client with api key from env var OPENAI_API_KEY and default base url.
-//! let client = Client::new();
+//! let client = Client::new(http_client::policy::IpPolicy::danger_always_allow());
 //!
 //! // Above is shortcut for
 //! let config = OpenAIConfig::default();
-//! let client = Client::with_config(config);
+//! let client = Client::with_config(http_client::policy::IpPolicy::danger_always_allow(), config);
 //!
 //! // OR use API key from different source and a non default organization
 //! let api_key = "sk-..."; // This secret could be from a file, or environment variable.
@@ -18,43 +18,14 @@
 //!     .with_api_key(api_key)
 //!     .with_org_id("the-continental");
 //!
-//! let client = Client::with_config(config);
+//! let client = Client::with_config(http_client::policy::IpPolicy::danger_always_allow(), config);
 //!
 //! // Use custom reqwest client
-//! let http_client = reqwest::ClientBuilder::new().user_agent("async-openai").build().unwrap();
-//! let client = Client::new().with_http_client(http_client);
+//! let http_client = http_client::reqwest::Client::builder().prepare(|client|client.user_agent("async-openai")).build_with_policies(http_client::policy::IpPolicy::danger_always_allow(), http_client::reqwest::redirect::Policy::default()).unwrap();
+//! let client = Client::new(http_client::policy::IpPolicy::danger_always_allow()).with_http_client(http_client);
 //! ```
 //!
 //!
-//! ## Making requests
-//!
-//!```
-//!# tokio_test::block_on(async {
-//!
-//! use async_openai::{Client, types::{CreateCompletionRequestArgs}};
-//!
-//! // Create client
-//! let client = Client::new();
-//!
-//! // Create request using builder pattern
-//! // Every request struct has companion builder struct with same name + Args suffix
-//! let request = CreateCompletionRequestArgs::default()
-//!     .model("gpt-3.5-turbo-instruct")
-//!     .prompt("Tell me the recipe of alfredo pasta")
-//!     .max_tokens(40_u32)
-//!     .build()
-//!     .unwrap();
-//!
-//! // Call API
-//! let response = client
-//!     .completions()      // Get the API "group" (completions, images, etc.) from the client
-//!     .create(request)    // Make the API call in that "group"
-//!     .await
-//!     .unwrap();
-//!
-//! println!("{}", response.choices.first().unwrap().text);
-//! # });
-//!```
 //!
 //! ## Bring Your Own Types
 //!
@@ -105,7 +76,7 @@
 //!     .with_deployment_id("deployment-id")
 //!     .with_api_key("...");
 //!
-//! let client = Client::with_config(config);
+//! let client = Client::with_config(http_client::policy::IpPolicy::danger_always_allow(), config);
 //!
 //! // Note that `async-openai` only implements OpenAI spec
 //! // and doesn't maintain parity with the spec of Azure OpenAI service.
