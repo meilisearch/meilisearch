0f0be96396d3dfad5e103ee10488325e9cebe074
ManyTheFish <many@meilisearch.com>
2026-01-27 17:54:02 +0100

    Remove dead code

--- crates/milli/src/update/new/words_prefix_docids.rs
+++ crates/milli/src/update/new/words_prefix_docids.rs
@@ -1,17 +1,14 @@
-use std::cell::RefCell;
 use std::collections::BTreeSet;
 use std::io::{BufReader, BufWriter, Read, Seek, Write};
 use std::iter;
 
 use hashbrown::HashMap;
 use heed::types::{Bytes, DecodeIgnore, Str};
-use heed::{BytesDecode, Database, Error, RoTxn, RwTxn};
+use heed::{Database, RwTxn};
 use rayon::iter::{IndexedParallelIterator as _, IntoParallelIterator, ParallelIterator as _};
 use roaring::MultiOps;
 use tempfile::spooled_tempfile;
-use thread_local::ThreadLocal;
 
-use super::ref_cell_ext::RefCellExt as _;
 use crate::heed_codec::StrBEU16Codec;
 use crate::update::GrenadParameters;
 use crate::{CboRoaringBitmapCodec, Index, Prefix, Result};
@@ -113,66 +110,6 @@ impl<'i> WordPrefixDocids<'i> {
 
         Ok(())
     }
-
-    #[tracing::instrument(level = "trace", skip_all, target = "indexing::prefix")]
-    fn recompute_modified_prefixes(
-        &self,
-        wtxn: &mut RwTxn,
-        prefixes: &BTreeSet<Prefix>,
-    ) -> Result<()> {
-        // We fetch the docids associated to the newly added word prefix fst only.
-        // And collect the CboRoaringBitmaps pointers in an HashMap.
-        let frozen = FrozenPrefixBitmaps::from_prefixes(self.database, wtxn, prefixes)?;
-
-        // We access this HashMap in parallel to compute the *union* of all
-        // of them and *serialize* them into files. There is one file by CPU.
-        let local_entries = ThreadLocal::with_capacity(rayon::current_num_threads());
-        prefixes.into_par_iter().map(AsRef::as_ref).try_for_each(|prefix| {
-            let refcell = local_entries.get_or(|| {
-                let file = BufWriter::new(spooled_tempfile(
-                    self.max_memory_by_thread.unwrap_or(usize::MAX),
-                ));
-                RefCell::new((Vec::new(), file, Vec::new()))
-            });
-
-            let mut refmut = refcell.borrow_mut_or_yield();
-            let (ref mut index, ref mut file, ref mut buffer) = *refmut;
-
-            let output = frozen
-                .bitmaps(prefix)
-                .unwrap()
-                .iter()
-                .map(|bytes| CboRoaringBitmapCodec::deserialize_from(bytes))
-                .union()?;
-
-            buffer.clear();
-            CboRoaringBitmapCodec::serialize_into_vec(&output, buffer);
-            index.push(PrefixEntry { prefix, serialized_length: buffer.len() });
-            file.write_all(buffer)
-        })?;
-
-        drop(frozen);
-
-        // We iterate over all the collected and serialized bitmaps through
-        // the files and entries to eventually put them in the final database.
-        for refcell in local_entries {
-            let (index, file, mut buffer) = refcell.into_inner();
-            let mut file = file.into_inner().map_err(|e| e.into_error())?;
-            file.rewind()?;
-            let mut file = BufReader::new(file);
-            for PrefixEntry { prefix, serialized_length } in index {
-                buffer.resize(serialized_length, 0);
-                file.read_exact(&mut buffer)?;
-                self.prefix_database.remap_data_type::<Bytes>().put(
-                    wtxn,
-                    prefix.as_bytes(),
-                    &buffer,
-                )?;
-            }
-        }
-
-        Ok(())
-    }
 }
 
 /// Represents a prefix and the lenght the bitmap takes on disk.
@@ -180,46 +117,6 @@ struct PrefixEntry<'a> {
     prefix: &'a str,
     serialized_length: usize,
 }
-
-/// Stores prefixes along with all the pointers to the associated
-/// CBoRoaringBitmaps.
-///
-/// They are collected synchronously and stored into an HashMap. The
-/// Synchronous process is doing a small amount of work by just storing
-/// pointers. It can then be accessed in parallel to get the associated
-/// bitmaps pointers.
-struct FrozenPrefixBitmaps<'a, 'rtxn> {
-    prefixes_bitmaps: HashMap<&'a str, Vec<&'rtxn [u8]>>,
-}
-
-impl<'a, 'rtxn> FrozenPrefixBitmaps<'a, 'rtxn> {
-    #[tracing::instrument(level = "trace", skip_all, target = "indexing::prefix")]
-    pub fn from_prefixes(
-        database: Database<Bytes, CboRoaringBitmapCodec>,
-        rtxn: &'rtxn RoTxn,
-        prefixes: &'a BTreeSet<Prefix>,
-    ) -> heed::Result<Self> {
-        let database = database.remap_data_type::<Bytes>();
-
-        let mut prefixes_bitmaps = HashMap::new();
-        for prefix in prefixes {
-            let mut bitmap_bytes = Vec::new();
-            for result in database.prefix_iter(rtxn, prefix.as_bytes())? {
-                let (_word, bytes) = result?;
-                bitmap_bytes.push(bytes);
-            }
-            assert!(prefixes_bitmaps.insert(prefix.as_str(), bitmap_bytes).is_none());
-        }
-
-        Ok(Self { prefixes_bitmaps })
-    }
-
-    pub fn bitmaps(&self, key: &str) -> Option<&[&'rtxn [u8]]> {
-        self.prefixes_bitmaps.get(key).map(AsRef::as_ref)
-    }
-}
-
-unsafe impl Sync for FrozenPrefixBitmaps<'_, '_> {}
 
 struct WordPrefixIntegerDocids<'i> {
     index: &'i Index,
@@ -372,87 +269,6 @@ impl<'i> WordPrefixIntegerDocids<'i> {
 
         Ok(())
     }
-
-    #[tracing::instrument(level = "trace", skip_all, target = "indexing::prefix")]
-    fn recompute_modified_prefixes(
-        &self,
-        wtxn: &mut RwTxn,
-        prefixes: &BTreeSet<Prefix>,
-    ) -> Result<()> {
-        // We fetch the docids associated to the newly added word prefix fst only.
-        // And collect the CboRoaringBitmaps pointers in an HashMap.
-        let frozen = FrozenPrefixIntegerBitmaps::from_prefixes(self.database, wtxn, prefixes)?;
-
-        // We access this HashMap in parallel to compute the *union* of all
-        // of them and *serialize* them into files. There is one file by CPU.
-        let local_entries = ThreadLocal::with_capacity(rayon::current_num_threads());
-        prefixes.into_par_iter().map(AsRef::as_ref).try_for_each(|prefix| -> Result<()> {
-            let refcell = local_entries.get_or(|| {
-                let file = BufWriter::new(spooled_tempfile(
-                    self.max_memory_by_thread.unwrap_or(usize::MAX),
-                ));
-                RefCell::new((Vec::new(), file, Vec::new()))
-            });
-
-            let mut refmut = refcell.borrow_mut_or_yield();
-            let (ref mut index, ref mut file, ref mut buffer) = *refmut;
-
-            for (&pos, bitmaps_bytes) in frozen.bitmaps(prefix).unwrap() {
-                if bitmaps_bytes.is_empty() {
-                    index.push(PrefixIntegerEntry { prefix, pos, serialized_length: None });
-                } else {
-                    let output = bitmaps_bytes
-                        .iter()
-                        .map(|bytes| CboRoaringBitmapCodec::deserialize_from(bytes))
-                        .union()?;
-                    buffer.clear();
-                    CboRoaringBitmapCodec::serialize_into_vec(&output, buffer);
-                    index.push(PrefixIntegerEntry {
-                        prefix,
-                        pos,
-                        serialized_length: Some(buffer.len()),
-                    });
-                    file.write_all(buffer)?;
-                }
-            }
-
-            Result::Ok(())
-        })?;
-
-        drop(frozen);
-
-        // We iterate over all the collected and serialized bitmaps through
-        // the files and entries to eventually put them in the final database.
-        let mut key_buffer = Vec::new();
-        for refcell in local_entries {
-            let (index, file, mut buffer) = refcell.into_inner();
-            let mut file = file.into_inner().map_err(|e| e.into_error())?;
-            file.rewind()?;
-            let mut file = BufReader::new(file);
-            for PrefixIntegerEntry { prefix, pos, serialized_length } in index {
-                key_buffer.clear();
-                key_buffer.extend_from_slice(prefix.as_bytes());
-                key_buffer.push(0);
-                key_buffer.extend_from_slice(&pos.to_be_bytes());
-                match serialized_length {
-                    Some(serialized_length) => {
-                        buffer.resize(serialized_length, 0);
-                        file.read_exact(&mut buffer)?;
-                        self.prefix_database.remap_data_type::<Bytes>().put(
-                            wtxn,
-                            &key_buffer,
-                            &buffer,
-                        )?;
-                    }
-                    None => {
-                        self.prefix_database.delete(wtxn, &key_buffer)?;
-                    }
-                }
-            }
-        }
-
-        Ok(())
-    }
 }
 
 /// Represents a prefix and the length the bitmap takes on disk.
@@ -461,41 +277,6 @@ struct PrefixIntegerEntry<'a> {
     pos: u16,
     serialized_length: Option<usize>,
 }
-
-/// TODO doc
-struct FrozenPrefixIntegerBitmaps<'a, 'rtxn> {
-    prefixes_bitmaps: HashMap<&'a str, HashMap<u16, Vec<&'rtxn [u8]>>>,
-}
-
-impl<'a, 'rtxn> FrozenPrefixIntegerBitmaps<'a, 'rtxn> {
-    #[tracing::instrument(level = "trace", skip_all, target = "indexing::prefix")]
-    pub fn from_prefixes(
-        database: Database<Bytes, CboRoaringBitmapCodec>,
-        rtxn: &'rtxn RoTxn,
-        prefixes: &'a BTreeSet<Prefix>,
-    ) -> heed::Result<Self> {
-        let database = database.remap_data_type::<Bytes>();
-
-        let mut prefixes_bitmaps = HashMap::new();
-        for prefix in prefixes {
-            let mut positions = HashMap::new();
-            for result in database.prefix_iter(rtxn, prefix.as_bytes())? {
-                let (key, bytes) = result?;
-                let (_word, pos) = StrBEU16Codec::bytes_decode(key).map_err(Error::Decoding)?;
-                positions.entry(pos).or_insert_with(Vec::new).push(bytes);
-            }
-            assert!(prefixes_bitmaps.insert(prefix.as_str(), positions).is_none());
-        }
-
-        Ok(Self { prefixes_bitmaps })
-    }
-
-    pub fn bitmaps(&self, key: &'a str) -> Option<&HashMap<u16, Vec<&'rtxn [u8]>>> {
-        self.prefixes_bitmaps.get(&key)
-    }
-}
-
-unsafe impl Sync for FrozenPrefixIntegerBitmaps<'_, '_> {}
 
 #[tracing::instrument(level = "trace", skip_all, target = "indexing::prefix")]
 fn delete_prefixes(
