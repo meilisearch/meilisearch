32e768bf6129b233106821fc37ae3eb5b275d3f6
YoEight <yo.eight@gmail.com>
2026-02-12 15:22:44 -0500

    add dynamic search rule data structure

--- crates/meilisearch-types/src/dynamic_search_rules.rs
+++ crates/meilisearch-types/src/dynamic_search_rules.rs
@@ -1,5 +1,112 @@
+use std::collections::BTreeMap;
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub type DynamicSearchRules = BTreeMap<String, DynamicSearchRule>;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct DynamicSearchRules {}
+pub struct DynamicSearchRule {
+    /// data format version to support upcasting if the format evolves over time.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub version: Option<String>,
+    pub uid: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub priority: Option<i64>,
+    #[serde(default)]
+    pub active: bool,
+    #[serde(default)]
+    pub conditions: Vec<Condition>,
+    pub actions: Vec<RuleAction>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(tag = "scope", content = "settings", rename_all = "camelCase")]
+pub enum Condition {
+    Query(QueryCondition),
+    Time(TimeCondition),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryCondition {
+    pub is_empty: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct TimeCondition {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub start: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub end: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RuleAction {
+    pub selector: Selector,
+    pub action: Action,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct Selector {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub index_uid: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub filter: Option<Filter>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct Filter {
+    pub attribute: String,
+    pub op: FilterOp,
+    pub value: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub enum FilterOp {
+    Eq,
+    Neq,
+    Gt,
+    Lt,
+    Gte,
+    Lte,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(tag = "name", content = "args", rename_all = "camelCase")]
+pub enum Action {
+    Pin(PinArgs),
+    Boost(BoostArgs),
+    Bury(BuryArgs),
+    Hide(HideArgs),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct PinArgs {
+    pub position: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct BoostArgs {
+    pub score: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct BuryArgs {
+    pub score: f64,
+}
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct HideArgs {}
