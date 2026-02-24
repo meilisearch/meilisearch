81a7733095f840c2a4b4c92576c311ec06e50f36
Louis Dureuil <louis@meilisearch.com>
2026-02-24 08:57:44 +0100

    Make sure all shards are created on update

--- crates/milli/src/update/upgrade/v1_37.rs
+++ crates/milli/src/update/upgrade/v1_37.rs
@@ -72,17 +72,20 @@ impl super::UpgradeIndex for AddShards {
             return Ok(false);
         };
 
-        // before this upgrade, there is at most one shard owned by the remote.
-        // if we find it, we can associate all docids to that shard.
-        let Some(own_shard) = shards.as_sorted_slice().iter().find(|shard| shard.is_own) else {
-            return Ok(false);
-        };
-
         let shard_docids = index.shard_docids();
 
-        let docids = index.documents_ids(wtxn)?;
+        for shard in shards.as_sorted_slice() {
+            shard_docids.add_shard(wtxn, shard.name.as_str())?;
 
-        shard_docids.put_docids(wtxn, &own_shard.name, &docids)?;
+            // before this upgrade, there is at most one shard owned by the remote.
+            // if we find it, we can associate all docids to that shard.
+            if shard.is_own {
+                let docids = index.documents_ids(wtxn)?;
+
+                shard_docids.put_docids(wtxn, &shard.name, &docids)?;
+            }
+        }
+
         Ok(false)
     }
 
