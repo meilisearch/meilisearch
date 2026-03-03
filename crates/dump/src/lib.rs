e1ed1a9a5922aa6844ab9b79cabc1cad8d5c37ac
Clément Renault <clement@meilisearch.com>
2026-03-03 12:08:47 +0100

    Add and modify some tests

--- crates/dump/src/lib.rs
+++ crates/dump/src/lib.rs
@@ -494,7 +494,7 @@ pub(crate) mod test {
         ]
     }
 
-    pub fn create_test_dump() -> File {
+    pub fn create_test_dump_writer() -> (DumpWriter, BatchWriter) {
         let instance_uid = create_test_instance_uid();
         let dump = DumpWriter::new(Some(instance_uid)).unwrap();
 
@@ -516,7 +516,6 @@ pub(crate) mod test {
         for batch in &batches {
             batch_queue.push_batch(batch).unwrap();
         }
-        batch_queue.flush().unwrap();
 
         // ========== pushing the task queue
         let tasks = create_test_tasks();
@@ -549,6 +548,13 @@ pub(crate) mod test {
         // ========== network
         let network = create_test_network();
         dump.create_network(network).unwrap();
+
+        (dump, batch_queue)
+    }
+
+    pub fn create_test_dump() -> File {
+        let (dump, batch_writer) = create_test_dump_writer();
+        batch_writer.flush().unwrap();
 
         // create the dump
         let mut file = tempfile::tempfile().unwrap();
