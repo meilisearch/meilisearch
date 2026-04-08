72307dac80e07cad940af7de284a29d88dbce5dc
Kerollmops <clement@meilisearch.com>
2026-04-08 12:12:08 +0200

    Expose the delta-encoding codecs outside

--- crates/milli/src/lib.rs
+++ crates/milli/src/lib.rs
@@ -77,10 +77,10 @@ pub use self::filterable_attributes_rules::{
     FilterableAttributesRule,
 };
 pub use self::foreign_key::ForeignKey;
+use self::heed_codec::RoaringBitmapLenCodec;
 pub use self::heed_codec::{
-    BEU16StrCodec, BEU32StrCodec, BoRoaringBitmapCodec, BoRoaringBitmapLenCodec,
-    CboRoaringBitmapCodec, CboRoaringBitmapLenCodec, FieldIdWordCountCodec, ObkvCodec,
-    RoaringBitmapCodec, RoaringBitmapLenCodec, StrBEU32Codec, U8StrStrCodec,
+    BEU16StrCodec, BEU32StrCodec, DeCboRoaringBitmapCodec, DeCboRoaringBitmapLenCodec,
+    FieldIdWordCountCodec, ObkvCodec, RoaringBitmapCodec, StrBEU32Codec, U8StrStrCodec,
     UncheckedU8StrStrCodec,
 };
 pub use self::index::{CreateOrOpen, Index};
