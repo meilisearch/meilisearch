6eb266bda5196f4b99d789058d349278def5e3ea
Clément Renault <clement@meilisearch.com>
2026-04-14 15:14:02 +0200

    PatternMatch::Parent cannot happen on reserved single field names (_geo, _geoJSON)

--- crates/milli/src/update/new/indexer/mod.rs
+++ crates/milli/src/update/new/indexer/mod.rs
@@ -268,14 +268,12 @@ where
     )?;
 
     // Clear the cellulite database when the geojson support is removed
-    // TODO what about PatternMatch::Parent and please test that cellulite is cleared correctly
     if settings_delta.new_match_faceted_field(RESERVED_GEOJSON_FIELD_NAME) == PatternMatch::NoMatch
     {
         index.cellulite.clear(wtxn)?;
     }
 
     // Clear the geo rtree entry when the geo support is removed
-    // TODO what about PatternMatch::Parent and please test that the geo rtree entry is cleared correctly
     if settings_delta.new_match_faceted_field(RESERVED_GEO_FIELD_NAME) == PatternMatch::NoMatch {
         index.delete_geo_rtree(wtxn)?;
     }
