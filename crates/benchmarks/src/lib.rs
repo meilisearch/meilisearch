2da5ed797a85ff3249412109f84d497ff14895b2
Kerollmops <clement@meilisearch.com>
2026-06-01 12:04:07 +0200

    Remove milli's benchmarks from the workspace

--- crates/benchmarks/src/lib.rs
+++
@@ -1,5 +0,0 @@
-//! This library is only used to isolate the benchmarks
-//! from the original milli library.
-//!
-//! It does not include interesting functions for milli library
-//! users only for milli contributors.
