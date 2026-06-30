48b69e6d554176d038742fe3b4eec5c1bba73aac
Louis Dureuil <louis@meilisearch.com>
2026-06-30 16:37:45 +0200

    DSR type changes

--- crates/meilisearch-types/src/dynamic_search_rules.rs
+++ crates/meilisearch-types/src/dynamic_search_rules.rs
@@ -17,7 +17,6 @@ use crate::error::deserr_codes::{
 };
 use crate::error::ParseOffsetDateTimeError;
 use crate::index_uid::IndexUid;
-use crate::locales::Locale;
 
 pub type RuleUid = IndexUid;
 
@@ -36,13 +35,13 @@ pub struct DynamicSearchRule {
     /// - If the same document is selected by multiple rules, the smallest `priority` number wins
     /// - If different documents are pinned to the same position, they are ordered by ascending `priority`
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub priority: Option<u64>,
+    pub precedence: Option<u64>,
     /// Whether the dynamic search rule is active.
     #[serde(default = "default_dynamic_search_rule_active")]
     pub active: bool,
     /// Conditions that must match before the dynamic search rule applies.
     #[serde(default)]
-    pub conditions: Vec<Condition>,
+    pub conditions: Conditions,
     /// Actions to apply when the dynamic search rule matches.
     pub actions: Vec<RuleAction>,
 }
@@ -51,93 +50,121 @@ const fn default_dynamic_search_rule_active() -> bool {
     true
 }
 
-#[derive(Serialize, Deserialize, Deserr, Debug, Clone, PartialEq, Eq, ToSchema)]
-#[deserr(tag = "scope", rename_all = camelCase, validate = validate_condition -> DeserrJsonError)]
-#[serde(tag = "scope", rename_all = "camelCase")]
-#[schema(rename_all = "camelCase")]
-pub enum Condition {
-    #[deserr(rename_all = camelCase)]
-    #[serde(rename_all = "camelCase")]
-    Query {
-        #[serde(default, skip_serializing_if = "Option::is_none")]
-        #[deserr(default)]
-        is_empty: Option<bool>,
-        #[serde(default, skip_serializing_if = "Option::is_none")]
-        #[deserr(default)]
-        contains: Option<String>,
-    },
-
-    #[deserr(rename_all = camelCase)]
-    #[serde(rename_all = "camelCase")]
-    Time {
-        #[serde(
-            default,
-            skip_serializing_if = "Option::is_none",
-            with = "time::serde::rfc3339::option"
-        )]
-        #[deserr(default, try_from(Option<String>) = parse_optional_rfc3339_datetime -> ParseOffsetDateTimeError)]
-        start: Option<OffsetDateTime>,
-        #[serde(
-            default,
-            skip_serializing_if = "Option::is_none",
-            with = "time::serde::rfc3339::option"
-        )]
-        #[deserr(default, try_from(Option<String>) = parse_optional_rfc3339_datetime -> ParseOffsetDateTimeError)]
-        end: Option<OffsetDateTime>,
-    },
+#[routes::request(db, setting)]
+#[derive(Debug, Clone, PartialEq)]
+pub struct DynamicSearchRuleUpdateRequest {
+    /// Human-readable description of the dynamic search rule.
+    #[request(default, error = DeserrJsonError<InvalidDynamicSearchRuleDescription>, schema_type = Option<String>, skip_serializing_if = "Setting::is_not_set")]
+    pub description: Setting<String>,
+    /// Precedence of the dynamic search rule. Lower numeric values take precedence over higher
+    /// ones. If omitted, the rule is treated as having the lowest precedence. This precedence is
+    /// used to resolve conflicts between matching rules:
+    /// - If the same document is selected by multiple rules, the smallest `priority` number wins
+    /// - If different documents are pinned to the same position, they are ordered by ascending `priority`
+    #[request(default, error = DeserrJsonError<InvalidDynamicSearchRulePriority>, schema_type = Option<u64>, skip_serializing_if = "Setting::is_not_set")]
+    pub precedence: Setting<u64>,
+    /// Whether the dynamic search rule is active.
+    #[request(default, error = DeserrJsonError<InvalidDynamicSearchRuleActive>, schema_type = Option<bool>, skip_serializing_if = "Setting::is_not_set")]
+    pub active: Setting<bool>,
+    /// Conditions that must match before the dynamic search rule applies.
+    #[request(default, error = DeserrJsonError<InvalidDynamicSearchRuleConditions>, schema_type = Option<Conditions>, skip_serializing_if = "Setting::is_not_set")]
+    pub conditions: Setting<Conditions>,
+    /// Actions to apply when the dynamic search rule matches.
+    #[request(default, error = DeserrJsonError<InvalidDynamicSearchRuleActions>, schema_type = Option<Vec<RuleAction>>, skip_serializing_if = "Setting::is_not_set")]
+    pub actions: Setting<Vec<RuleAction>>,
 }
 
-// manual impl: no support for `with` and `tag`
-impl routes::RequestBody for Condition {}
+#[routes::request(db, validate = validate_condition -> DeserrJsonError, override_error = DeserrJsonError<InvalidDynamicSearchRuleConditions>)]
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct Conditions {
+    /// Time range where the rule is active
+    #[request(default, skip_serializing_if = "Option::is_none")]
+    pub time: Option<TimeCondition>,
+    /// Conditions on the search query that determines whether the rule is active
+    #[request(default, skip_serializing_if = "Option::is_none")]
+    pub query: Option<QueryCondition>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, ToSchema, Deserr)]
+#[deserr(rename_all = camelCase)]
+#[serde(rename_all = "camelCase")]
+#[schema(rename_all = "camelCase")]
+pub struct TimeCondition {
+    #[serde(
+        default,
+        skip_serializing_if = "Option::is_none",
+        with = "time::serde::rfc3339::option"
+    )]
+    #[deserr(default, try_from(Option<String>) = parse_optional_rfc3339_datetime -> ParseOffsetDateTimeError)]
+    /// Start of the time range where this rule can be considered active.
+    ///
+    /// Specify as a RFC3339 datetime.
+    pub start: Option<OffsetDateTime>,
+    #[serde(
+        default,
+        skip_serializing_if = "Option::is_none",
+        with = "time::serde::rfc3339::option"
+    )]
+    /// End of the time range where this rule can be considered active.
+    ///
+    /// Specify as a RFC3339 datetime.
+    #[deserr(default, try_from(Option<String>) = parse_optional_rfc3339_datetime -> ParseOffsetDateTimeError)]
+    pub end: Option<OffsetDateTime>,
+}
+
+impl routes::RequestBody for TimeCondition {}
+
+#[routes::request(db, override_error = DeserrJsonError<InvalidDynamicSearchRuleConditions>)]
+#[derive(Debug, Clone, PartialEq)]
+pub struct QueryCondition {
+    /// If present and non-null, specifies either:
+    ///
+    /// - That this rule can only be active when the search query is empty
+    /// - That this rule can only be active when the search query is non-empty (contains at least one word)
+    #[request(default, skip_serializing_if = "Option::is_none")]
+    pub is_empty: Option<bool>,
+
+    /// If present and non-null, specifies that the rule can only be active if all the specified words are
+    /// present in the search query.
+    #[request(default, skip_serializing_if = "Option::is_none")]
+    pub words: Option<String>,
+}
 
 // We manually check the exclusivity of `is_empty` and `contains` because Deserr does not support
 // untagged enums
 fn validate_condition<E: DeserializeError>(
-    condition: Condition,
+    conditions: Conditions,
     location: ValuePointerRef,
-) -> Result<Condition, E> {
-    match &condition {
-        Condition::Query { is_empty, contains } => {
-            if is_empty.is_some() && contains.is_some() {
-                return Err(deserr::take_cf_content(E::error::<Infallible>(
-                    None,
-                    ErrorKind::Unexpected {
-                        msg: "either `isEmpty` or `contains` can be used, not all at once"
-                            .to_string(),
-                    },
-                    location,
-                )));
-            }
-
-            if is_empty.is_none() && contains.is_none() {
-                return Err(deserr::take_cf_content(E::error::<Infallible>(
-                    None,
-                    ErrorKind::Unexpected {
-                        msg: "at least `isEmpty` or `contains` must be used".to_string(),
-                    },
-                    location,
-                )));
-            }
+) -> Result<Conditions, E> {
+    // 1. check is_empty and words
+    if let Some(query) = &conditions.query {
+        if query.is_empty == Some(true) && query.words.is_some() {
+            return Err(deserr::take_cf_content(E::error::<Infallible>(
+                None,
+                ErrorKind::Unexpected {
+                    msg: "either `isEmpty` or `words` can be used, not both at once".to_string(),
+                },
+                location.push_key("query"),
+            )));
         }
+    }
 
-        Condition::Time { start, end } => {
-            if let Some((start, end)) = start.as_ref().zip(end.as_ref()) {
-                if start > end {
-                    return Err(deserr::take_cf_content(E::error::<Infallible>(
-                        None,
-                        ErrorKind::Unexpected {
-                            msg: format!(
-                                "`end` (`{end}`) should be later than `start` (`{start}`)"
-                            ),
-                        },
-                        location,
-                    )));
-                }
+    // 2. check that start is before end
+    if let Some(time) = &conditions.time {
+        if let Some((start, end)) = time.start.as_ref().zip(time.end.as_ref()) {
+            if start > end {
+                return Err(deserr::take_cf_content(E::error::<Infallible>(
+                    None,
+                    ErrorKind::Unexpected {
+                        msg: format!("`end` (`{end}`) should be later than `start` (`{start}`)"),
+                    },
+                    location.push_key("time"),
+                )));
             }
         }
     }
 
-    Ok(condition)
+    Ok(conditions)
 }
 
 #[derive(Serialize, Deserialize, Deserr, Debug, Clone, PartialEq, ToSchema)]
@@ -173,9 +200,7 @@ pub struct Selector {
     #[serde(default, skip_serializing_if = "Option::is_none")]
     #[deserr(default)]
     pub index_uid: Option<IndexUid>,
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    #[deserr(default)]
-    pub id: Option<String>,
+    pub id: String,
 }
 
 #[derive(Serialize, Deserialize, Deserr, Debug, Clone, PartialEq, Eq, ToSchema)]
