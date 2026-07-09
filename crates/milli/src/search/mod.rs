624bdb627d202625fc645c57f7539f4a422322be
Louis Dureuil <louis@meilisearch.com>
2026-07-09 17:27:50 +0200

    Pass filter to DSR context

--- crates/milli/src/search/mod.rs
+++ crates/milli/src/search/mod.rs
@@ -302,7 +302,7 @@ impl<'a> Search<'a> {
             filtered_universe(ctx.index, ctx.txn, &self.filter, self.candidates, self.progress)?;
 
         let (query_terms, pins, used_negative_operator) =
-            self.build_located_query_terms(&mut ctx, &mut universe)?;
+            self.build_located_query_terms(&mut ctx, self.filter.as_ref(), &mut universe)?;
 
         let mut query_vector = None;
         let PartialSearchResult {
@@ -391,6 +391,7 @@ impl<'a> Search<'a> {
     pub fn build_located_query_terms(
         &self,
         ctx: &mut SearchContext<'_>,
+        filter: Option<&IndexFilter>,
         universe: &mut RoaringBitmap,
     ) -> Result<(Option<(QueryGraph, Vec<new::LocatedQueryTerm>)>, Vec<PinDoc>, bool), Error> {
         let mut used_negative_operator = false;
@@ -423,6 +424,7 @@ impl<'a> Search<'a> {
             .map(|(dsrs, fuel)| {
                 dsrs.resolve_pins(
                     query_graph_terms.as_ref().map(|(_, terms)| terms.as_slice()).unwrap_or(&[]),
+                    filter,
                     universe,
                     ctx,
                     fuel,
