c8af1ea06cdba287ded3ad2dd62b047638fd7df5
Clément Renault <clement@meilisearch.com>
2026-07-22 10:59:08 +0200

    Make sure legacy attribute patterns works

--- crates/milli/src/filterable_attributes_rules.rs
+++ crates/milli/src/filterable_attributes_rules.rs
@@ -4,7 +4,9 @@ use deserr::{DeserializeError, Deserr, ValuePointerRef};
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-use crate::attribute_patterns::{match_distinct_field, match_field_legacy, PatternMatch};
+use crate::attribute_patterns::{
+    match_distinct_field, match_field_legacy, match_pattern, PatternMatch,
+};
 use crate::constants::{RESERVED_GEOJSON_FIELD_NAME, RESERVED_GEO_FIELD_NAME};
 use crate::AttributePatterns;
 
@@ -29,13 +31,23 @@ impl FilterableAttributesRule {
         }
     }
 
+    /// Takes two patterns and returns true if one pattern matches another.
     pub fn intersect_patterns(&self, other: &str) -> bool {
         match self {
             // If the rule is a field, match the field against the provided pattern using the legacy behavior
-            FilterableAttributesRule::Field(field) => matches!(
-                match_field_legacy(other, field),
-                PatternMatch::Parent | PatternMatch::Match
-            ),
+            FilterableAttributesRule::Field(field) => {
+                let legacy = matches!(
+                    match_field_legacy(other, field),
+                    PatternMatch::Parent | PatternMatch::Match
+                );
+
+                let new = matches!(
+                    match_pattern(other, field),
+                    PatternMatch::Parent | PatternMatch::Match
+                );
+
+                legacy | new
+            }
             // If the rule is a pattern, match each one of them against the provided pattern using the new behavior
             FilterableAttributesRule::Pattern(patterns) => patterns.intersect_patterns(other),
         }
