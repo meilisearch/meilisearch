39eaab899ce592a5c6801e871e5b9bbd23baf876
Clément Renault <clement@meilisearch.com>
2026-07-28 16:25:04 +0200

    Replace synchronoise with a tokio broadcast channel

--- crates/index-scheduler/src/scheduler/mod.rs
+++ crates/index-scheduler/src/scheduler/mod.rs
@@ -24,7 +24,6 @@ mod test_failure;
 
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicU32, Ordering};
-use std::sync::Arc;
 
 use meilisearch_types::error::ResponseError;
 use meilisearch_types::heed::{Env, WithoutTls};
@@ -36,7 +35,6 @@ use process_batch::ProcessBatchInfo;
 use rayon::current_num_threads;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use roaring::RoaringBitmap;
-use synchronoise::SignalEvent;
 
 use crate::processing::{AtomicTaskStep, BatchProgress};
 use crate::{Error, IndexScheduler, IndexSchedulerOptions, Result, TickOutcome};
@@ -45,8 +43,11 @@ pub struct Scheduler {
     /// A boolean that can be set to true to stop the currently processing tasks.
     pub must_stop_processing: MustStopProcessing,
 
-    /// Get a signal when a batch needs to be processed.
-    pub(crate) wake_up: Arc<SignalEvent>,
+    /// Receive signals for when to process tasks or when anything changes in the tasks queue.
+    pub wake_up: tokio::sync::broadcast::Receiver<ModifiedTasks>,
+
+    /// Send signals to wake up the listening part and react to tasks changes.
+    pub waker: tokio::sync::broadcast::Sender<ModifiedTasks>,
 
     /// Whether auto-batching is enabled or not.
     pub(crate) autobatching_enabled: bool,
@@ -85,7 +86,8 @@ impl Scheduler {
     pub(crate) fn private_clone(&self) -> Self {
         Self {
             must_stop_processing: self.must_stop_processing.clone(),
-            wake_up: self.wake_up.clone(),
+            wake_up: self.wake_up.resubscribe(),
+            waker: self.waker.clone(),
             autobatching_enabled: self.autobatching_enabled,
             max_number_of_batched_tasks: self.max_number_of_batched_tasks,
             batched_tasks_size_limit: self.batched_tasks_size_limit,
@@ -127,10 +129,14 @@ impl Scheduler {
             dsr_fuel: _,
         } = options;
 
+        let (waker, wake_up) = tokio::sync::broadcast::channel(32);
+        // we want to start the loop right away in case meilisearch was ctrl+Ced while processing things
+        let _ = waker.send(ModifiedTasks::DumpImported).unwrap();
+
         Scheduler {
             must_stop_processing: MustStopProcessing::default(),
-            // we want to start the loop right away in case meilisearch was ctrl+Ced while processing things
-            wake_up: Arc::new(SignalEvent::auto(true)),
+            wake_up,
+            waker,
             autobatching_enabled: *autobatching_enabled,
             max_number_of_batched_tasks: *max_number_of_batched_tasks,
             batched_tasks_size_limit: *batched_tasks_size_limit,
@@ -260,6 +266,7 @@ impl IndexScheduler {
         progress.update_progress(BatchProgress::WritingTasksToDisk);
 
         processing_batch.finished();
+
         // whether the batch made progress.
         // a batch make progress if it failed or if it contains at least one fully processed (or cancelled) task.
         //
