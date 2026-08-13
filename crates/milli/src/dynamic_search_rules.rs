ce0fcdb8b2bded069d2f9934aea7acb06520e324
Kerollmops <clement@meilisearch.com>
2026-08-13 14:48:02 +0200

    Make clippy happy

--- crates/milli/src/dynamic_search_rules.rs
+++ crates/milli/src/dynamic_search_rules.rs
@@ -390,7 +390,8 @@ impl<'a> DynamicSearchRulesView<'a> {
                     k => {
                         for word_rules in words_rules.iter().combinations(k.into()) {
                             verifying_constraints_rules |= roaring::MultiOps::intersection(
-                                std::iter::once(&constraint_count_rules).chain(word_rules),
+                                std::iter::once(&constraint_count_rules)
+                                    .chain(word_rules),
                             );
                             if fuel.consume_word_combination().is_break() {
                                 break;
